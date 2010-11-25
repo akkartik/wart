9eb1d62c81aa7ff53b9d21a88fedc185a929cb10
Kartik K. Agaram <vc@akkartik.com>
2010-11-24 18:01:48 -0800

    32 - attempt at making map return type generic, but it fails because we haven't eval'd seqs yet. Just assume map returns a list.

--- 005core.lisp
+++ 005core.lisp
@@ -1,23 +1,11 @@
-(mac wc-let(var val . body)
-  `(call (fn(,var) ,@body) ,val))
-(special-form let wc-let)
-
 (mac wc-if args
   (if (oddp (length args)) ; there's an else
     `(cond ,@(tuples (insert-t-in-penultimate-position args) 2))
     `(cond ,@(tuples args 2))))
 (special-form if wc-if)
 
-(mac each(var vals . body)
-  `(loop for ,var in ,vals do ,@body))
-
 (mac wc-do args `(call (fn() ,@args)))
 (special-form do wc-do)
-
-; with can be a macro since we aren't overriding an existing keyword
-(defmacro with(binds &body body)
-  `(let ,(tuples binds 2)
-     ,@body))
 
 
 
