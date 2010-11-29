3eeaea28ab83751893ec789ed20f186f08c3dac8
Kartik K. Agaram <vc@akkartik.com>
2010-11-29 09:59:19 -0800

    83 - new defcoerce macro. Now val* is just internal to coerce.lisp.
    A new convention is starting to take shape; wart variants with a trailing * are often like the underlying version
    but do something different around quoting. compose* and call* quote some arguments. type* and val* reach 'into' quoted args.
    
    type* and val* are internal. Do they need such short names? Never mind, we'll get warned on name collisions.

--- 005coerce.lisp
+++ 005coerce.lisp
@@ -11,16 +11,26 @@
 (defun wart-coerce(val dest)
   (or (aand (gethash dest *wart-coercions*)
             (gethash (type* val) it)
-            (funcall it val))
+            (funcall it (val* val)))
       (funcall #'coerce val dest)))
 (defover coerce wart-coerce)
+
+(defmacro defcoerce(src dest converter)
+  `(progn
+     (unless (gethash ,dest *wart-coercions*)
+       (setf (gethash ,dest *wart-coercions*) (table)))
+     (setf (gethash ,src (gethash ,dest *wart-coercions*))
+           ,converter)))
+
+
+
+;; Internals
 
 (defun type*(val)
   (if (and (isa val 'symbol) (not (fboundp val)))
     (type* (eval val))
     (type-of val)))
 
-; Useful when creating new coercions.
 (defun val*(val)
   (if (isa val 'symbol)
     (val* (eval val))
