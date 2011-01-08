b84265cfe63433d7509947d84a9c64d4863f4583
Kartik K. Agaram <vc@akkartik.com>
2011-01-08 09:13:51 -0800

    160 - compose for macros by code-generating call* expressions. One test passes; now let's see how brittle this is.

--- 008ssyntaxes.lisp
+++ 008ssyntaxes.lisp
@@ -15,10 +15,26 @@
 (defmacro complement*(f)
   `(complement (fslot ,f)))
 
+(defmacro macro-composition-p(expr)
+  `(and (consp ,expr)
+        (is 'compose* (car ,expr))
+        (or (macp (cadr ,expr))
+            (macp (caddr ,expr)))))
+
+(defun expand-composition(f g args)
+  (if (macp f)
+    (if (macp g)
+      `(,f (,g ,@args))
+      `(,f (call (fslot ,g) ,@args)))
+    (if (macp g)
+      `(call (fslot ,f) (,g ,@args))
+      '*error*)))
+
 (defmacro call*(f &rest args)
   (cond
     ((macp f)  `(,f ,@args))
     ((and (atom f) (fboundp f))  `(call (function ,f) ,@args))
+    ((macro-composition-p f)   (expand-composition (cadr f) (caddr f) args))
     (t  `(call ,f ,@args))))
 
 (defmacro call*-quoted(a b)
