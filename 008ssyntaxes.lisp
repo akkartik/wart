5f17036bbcd05c848d1f3f1093d0ec88254025ef
Kartik K. Agaram <vc@akkartik.com>
2011-01-08 19:00:50 -0800

    161 - seeing light at the end of the tunnel, but first save what I had overnight.

--- 008ssyntaxes.lisp
+++ 008ssyntaxes.lisp
@@ -15,21 +15,35 @@
 (defmacro complement*(f)
   `(complement (fslot ,f)))
 
+(defun macro-or-compose(f)
+  (or (macp f)
+      (and (consp f)
+           (is 'compose* (car f)))))
+
 (defmacro macro-composition-p(expr)
   `(and (consp ,expr)
-        (is 'compose* (car ,expr))
-        (or (macp (cadr ,expr))
-            (macp (caddr ,expr)))))
+        (is 'compose* (car ,expr))))
 
+; flatten -> process from right to left
+; g is never nested compose
 (defun expand-composition(f g args)
+;?   (if (and (not (macp g))
+;?            (not (macro-composition-p g)))
+;?     (if (macp f)
+;?       `(,f (call (fslot ,g) ,@args))
   (if (macp f)
     (if (macp g)
       `(,f (,g ,@args))
       `(,f (call (fslot ,g) ,@args)))
     (if (macp g)
       `(call (fslot ,f) (,g ,@args))
-      '*error*)))
+      `(call (compose* ,f ,g) ,@args))))
 
+; needs to know about compose*
+;   1+^h.1 => (call (compose* '1+ 'h) 1)
+; but
+;   ++^h.1 => (incf (call h 1))
+; ssyntax doesn't seem a good fit for a lisp-2.
 (defmacro call*(f &rest args)
   (cond
     ((macp f)  `(,f ,@args))
