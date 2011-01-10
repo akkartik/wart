3f1c2875e88e229dcaf59b06de88f0d01884be9a
Kartik K. Agaram <vc@akkartik.com>
2011-01-09 20:04:36 -0800

    167 - more from arc.arc

--- 000helpers.lisp
+++ 000helpers.lisp
@@ -2,9 +2,19 @@
   `(handler-bind (#+sbcl(sb-kernel:redefinition-warning 'muffle-warning))
      ,@body))
 
+(defun pr(arg)
+  (format t "~a" arg))
+
+(defun prn(&rest args)
+  (map 'list 'pr args)
+  (format t "~%")
+  (car args))
+
 (defmacro synonym(&rest args)
   `(setf ,@(mapcar (lambda (x) `(symbol-function ',x))
                    args)))
+
+
 
 (synonym no null
          call funcall ; bootstrap version
@@ -21,17 +31,32 @@
   (and (symbolp f)
        (macro-function f)))
 
-(defun pr(arg)
-  (format t "~a" arg))
-
-(defun prn(&rest args)
-  (map 'list 'pr args)
-  (format t "~%")
-  (car args))
+; TODO: bound
 
 (defun idfn(x) x)
 
 (defun isnt(x y) (no (is x y)))
+
+(defmacro while(test &body body) ; define before we defover do
+  `(loop while ,test do ,@body))
+
+(defmacro thunk(&body body)
+  `(lambda() ,@body))
+
+(defmacro aand(&rest args)
+  (cond
+    ((no args)   t)
+    ((no (cdr args))   (car args))
+    (`(let ((it ,(car args)))   (and it (aand ,@(cdr args)))))))
+
+(defun match(a b)
+  (or (is a b)
+      (is b '_)
+      (and (consp a) (consp b)
+           (match (car a) (car b))
+           (match (cdr a) (cdr b)))))
+
+
 
 (synonym len length
          rev reverse
@@ -66,19 +91,6 @@
   (if (or (= n 0) (no xs))
       nil
       (cons (car xs) (firstn (1- n) (cdr xs)))))
-
-(defmacro aand(&rest args)
-  (cond
-    ((no args)   t)
-    ((no (cdr args))   (car args))
-    (`(let ((it ,(car args)))   (and it (aand ,@(cdr args)))))))
-
-(defun match(a b)
-  (or (is a b)
-      (is b '_)
-      (and (consp a) (consp b)
-           (match (car a) (car b))
-           (match (cdr a) (cdr b)))))
 
 
 
