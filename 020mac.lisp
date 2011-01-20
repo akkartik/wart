c92ff4fe41f705e47836b5c1fa6fffe47aa477c9
Kartik K. Agaram <vc@akkartik.com>
2011-01-20 08:52:44 -0800

    253 - ah, let was getting expanded into wart-let.

--- 020mac.lisp
+++ 020mac.lisp
@@ -1,16 +1,21 @@
 ;; Macros with complex arg lists and o$vars params that are guaranteed to be eval'd exactly once.
 
-(defmacro$ mac0(name params &body body)
-  `(defmacro$ ,name(&rest ,$args)
-     ,(compile-params params $args body)))
-
+;; Use let* everywhere here because wart will soon override let
 (defmacro$ mac(name params &body body)
   (wt-transform
     (let* (($os   (remove-if-not #'odollar-symbol-p (strip-rest params)))
            ($s  (mapcar #'odollar-symbol-to-dollar-symbol $os)))
       `(mac0 ,name ,params
-         `(let ,(mapcar #'list (list ,@$s) (list ,@$os))
+         `(let* ,(mapcar #'list (list ,@$s) (list ,@$os))
             ,(progn ,@body))))))
+
+
+
+;; Internals
+
+(defmacro$ mac0(name params &body body)
+  `(defmacro$ ,name(&rest ,$args)
+     ,(compile-params params $args body)))
 
 (defun odollar-symbol-p(s)
   (and (symbolp s)
