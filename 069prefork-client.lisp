6ed751d6c4eb312f8c996e8456e63b09aa8c2085
Kartik K. Agaram <vc@akkartik.com>
2011-02-02 11:51:01 -0800

    353 - ok, get rid of our example client/server

--- 069prefork-client.lisp
+++
@@ -1,7 +0,0 @@
-;; http://common-lisp.net/pipermail/small-cl-src/2004-June/000065.html
-
-(def client()
-  (connecting stream :at 1978
-    (format stream "PID ~D~%" (getpid))
-    (flush stream)
-    (read-line stream)))
