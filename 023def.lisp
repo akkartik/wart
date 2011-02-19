dbf1ba564fe22f8c801d5ade9a280518e3a5c37d
Kartik K. Agaram <vc@akkartik.com>
2011-02-18 18:54:11 -0800

    460 - global table of function signatures

--- 023def.lisp
+++ 023def.lisp
@@ -1,6 +1,11 @@
+(setf wart-signatures* (table))
+
 (defmacro$ def(name params &body body)
-  `(defun$ ,name(&rest ,$args)
-     ,(compile-params params $args body)))
+  `(progn
+    (setf (gethash ',name wart-signatures*)
+          ',params)
+    (defun$ ,name(&rest ,$args)
+      ,(compile-params params $args body))))
 
 (defmacro proc(name args . body)
   `(def ,name ,args ,@body nil))
