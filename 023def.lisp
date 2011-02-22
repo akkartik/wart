af2df9cd11963ad22027a169bf2c0ea3bf3333ba
Kartik K. Agaram <vc@akkartik.com>
2011-02-22 13:42:14 -0800

    470 - starting on def :case. Tests failing.

--- 023def.lisp
+++ 023def.lisp
@@ -2,10 +2,30 @@
 
 (defmacro$ def(name params &body body)
   `(progn
-    (setf (gethash ',name wart-signatures*)
-          ',params)
+    (unless (gethash ',name wart-signatures*)
+      (setf (gethash ',name wart-signatures*)
+            (list nil)))
+    (push (list (args-matcher ',params) (fn ,params ,@body))
+          (gethash ',name wart-signatures*))
     (defun$ ,name(&rest ,$args)
-      ,(compile-params params $args body))))
+      (call-correct-variant (gethash ',name wart-signatures*)
+                            ,$args))))
 
 (defmacro proc(name args . body)
   `(def ,name ,args ,@body nil))
+
+
+
+;; Internals
+
+(defun call-correct-variant(variants args)
+  (if (car variants)
+    (destructuring-bind (test func) (car variants)
+      (if (apply test args)
+        (apply func args)
+        (call-correct-variant (cdr variants) args)))))
+
+(defun args-matcher(params)
+  (let* ((min-args  (len (required-params params))))
+    (fn args
+      (>= (len args)  min-args))))
