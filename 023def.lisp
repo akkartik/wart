bfbe95e32a1644cc3372c59da0104a248c105fd8
Kartik K. Agaram <vc@akkartik.com>
2011-02-23 00:27:42 -0800

    476

--- 023def.lisp
+++ 023def.lisp
@@ -1,14 +1,19 @@
 (setf wart-signatures* (table))
 
 (defmacro$ def(name params &body body)
-  `(progn
-    (push (list (args-matcher ',params)
-                (fn ,params
-                  (handling-$vars ,@body)))
-          (gethash ',name wart-signatures*))
-    (defun$ ,name(&rest ,$args)
-      (call-correct-variant (gethash ',name wart-signatures*)
-                            ,$args))))
+  (destructuring-bind (test &rest body)   (if (iso :case (car body))
+                                            (cdr body)
+                                            (cons t body))
+    `(progn
+      (push (list (fn args
+                    (and (apply (length-matcher ',params) args)
+                         (apply (fn ,params ,test) args)))
+                  (fn ,params
+                    (handling-$vars ,@body)))
+            (gethash ',name wart-signatures*))
+      (defun$ ,name(&rest ,$args)
+        (call-correct-variant (gethash ',name wart-signatures*)
+                              ,$args)))))
 
 (defmacro proc(name args . body)
   `(def ,name ,args ,@body nil))
@@ -25,7 +30,7 @@
         (apply func args)
         (call-correct-variant (cdr variants) args)))))
 
-(defun args-matcher(params)
+(defun length-matcher(params)
   (fn args
     (>= (length (remove-if [kwargp params _] args)) ; len will be overridden
         (length (required-params params)))))
