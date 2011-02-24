4cec7fed99b0f6d20de89cedcfa8dd6ffa2466a1
Kartik K. Agaram <vc@akkartik.com>
2011-02-23 18:06:24 -0800

    491

--- 023def.lisp
+++ 023def.lisp
@@ -1,19 +1,31 @@
 (setf wart-signatures* (table))
 
+(defmacro$ def-case(name params test &body body)
+  `(progn
+    (push (list (fn args
+                  (and (apply (length-matcher ',params) args)
+                       (apply (fn ,params ,test) args)))
+                (fn ,params
+                  (handling-$vars ,@body)))
+          (gethash ',name wart-signatures*))
+    (defun$ ,name(&rest ,$args)
+      (call-correct-variant (gethash ',name wart-signatures*)
+                            ,$args))))
+
+(defmacro$ def-normal(name params &body body)
+  `(progn
+    (push (list (length-matcher ',params)
+                (fn ,params
+                  (handling-$vars ,@body)))
+          (gethash ',name wart-signatures*))
+    (defun$ ,name(&rest ,$args)
+      (call-correct-variant (gethash ',name wart-signatures*)
+                            ,$args))))
+
 (defmacro$ def(name params &body body)
-  (destructuring-bind (test &rest body)   (if (iso :case (car body))
-                                            (cdr body)
-                                            (cons t body))
-    `(progn
-      (push (list (fn args
-                    (and (apply (length-matcher ',params) args)
-                         (apply (fn ,params ,test) args)))
-                  (fn ,params
-                    (handling-$vars ,@body)))
-            (gethash ',name wart-signatures*))
-      (defun$ ,name(&rest ,$args)
-        (call-correct-variant (gethash ',name wart-signatures*)
-                              ,$args)))))
+  (case (car body)
+    (:case  `(def-case ,name ,params ,(cadr body) ,@(cddr body)))
+    (otherwise `(def-normal ,name ,params ,@body))))
 
 (defmacro proc(name args . body)
   `(def ,name ,args ,@body nil))
