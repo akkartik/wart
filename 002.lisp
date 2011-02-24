5ef69e675646f20e9fd24a93d4f37e179fd5316e
Kartik K. Agaram <vc@akkartik.com>
2011-02-23 17:55:12 -0800

    490 - more readable but slightly less efficient match

--- 002.lisp
+++ 002.lisp
@@ -8,9 +8,10 @@
   (or (equal a b)
       (equal b '_)
       (and (consp a) (consp b)
-           (or (equal (car b) '_..)
-               (and (match (car a) (car b))
-                    (match (cdr a) (cdr b)))))))
+           (equal (car b) '_..))
+      (and (consp a) (consp b)
+           (match (car a) (car b))
+           (match (cdr a) (cdr b)))))
 
 (defmacro while(test &body body) ; define before we defover do
   `(loop while ,test do ,@body))
