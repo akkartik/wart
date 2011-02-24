115a0fa1f1010dffe64672feec6046f7d661e88b
Kartik K. Agaram <vc@akkartik.com>
2011-02-23 17:49:32 -0800

    489 - now matches multiple list elements against '_..
    match starting to grow complex.

--- 002.lisp
+++ 002.lisp
@@ -8,8 +8,9 @@
   (or (equal a b)
       (equal b '_)
       (and (consp a) (consp b)
-           (match (car a) (car b))
-           (match (cdr a) (cdr b)))))
+           (or (equal (car b) '_..)
+               (and (match (car a) (car b))
+                    (match (cdr a) (cdr b)))))))
 
 (defmacro while(test &body body) ; define before we defover do
   `(loop while ,test do ,@body))
