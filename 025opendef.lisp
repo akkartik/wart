ebdf2f57a34e9897a9c214c538464a449611aa7a
Kartik K. Agaram <vc@akkartik.com>
2011-02-24 16:51:30 -0800

    512

--- 025opendef.lisp
+++ 025opendef.lisp
@@ -1,11 +1,12 @@
 ;; def and mac that can be augmented rather than just overridden.
 
 (extend-macro def(name params &body body) :if (iso :case (car body))
-  `(push (list (allf (length-matcher ',params)
-                     (fn ,params ,(cadr body)))
-               (fn ,params
-                 (handling-$vars ,@(cddr body))))
-         (gethash ',name wart-signatures*)))
+  `(prog1 ',name
+     (push (list (allf (length-matcher ',params)
+                       (fn ,params ,(cadr body)))
+                 (fn ,params
+                   (handling-$vars ,@(cddr body))))
+           (gethash ',name wart-signatures*))))
 
 (extend-macro mac(name params &body body) :if (iso :case (car body))
   `(extend-macro ,name ,params :if ,(cadr body) ,@(cddr body)))
