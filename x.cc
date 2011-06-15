e73b06f6547fbf54d1afc49cd8ac370dc618eeee
Kartik K. Agaram <vc@akkartik.com>
2011-06-15 09:51:47 -0700

    0 - test harness

---
+++ x.cc
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<string>
+
+int numFailures = 0;
+
+#define check(X) if (!(X)) { ++numFailures; fprintf(stderr, "F %s: %s\n", __FUNCTION__, #X); } \
+  else { fprintf(stderr, "."); fflush(stderr); }
+
+
+
+void test1() {
+  check(2+1 == 2);
+}
+
+void test2() {
+  check(1+1 == 2);
+}
+
+
+
+typedef void (*testfunc)(void);
+
+const testfunc tests[] = {test1, test2,};
+
+void runTests() {
+  for (int i = 0; i < sizeof(tests)/sizeof(tests[0]); ++i) {
+    (*tests[i])();
+  }
+  fprintf(stderr, "\n");
+  if (numFailures > 1) {
+    fprintf(stderr, "%d failures\n", numFailures);
+  }
+  else if (numFailures == 1) {
+    fprintf(stderr, "1 failure\n");
+  }
+}
+
+int main(int argc, char* argv[]) {
+  if (argc == 1) return 0;
+  std::string arg1(argv[1]);
+  if (arg1 == "test") {
+    runTests();
+  }
+  return 0;
+}
