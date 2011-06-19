472f6904c3e1f50225326c66d8e1978221ae216b
Kartik K. Agaram <vc@akkartik.com>
2011-06-18 20:27:22 -0700

    118 - superficial cleanup

--- x.cc
+++ x.cc
@@ -554,24 +554,12 @@ void test_slurpLine_includes_indent_from_next_line() {
                                     return (*q == L"(");
                                   }
 
-                                  bool wrappingLine(list<Token>::iterator p) {
-                                    if (*p == START_OF_LINE) ++p;
-                                    cerr << "here: " << p->type << endl;
-                                    return (*p == MAYBE_WRAP);
-                                  }
-
 list<Token> parenthesize(list<Token> in) {
   list<Token> result;
   list<int> parenStack;
   int suppressInsert = 0;
   int nextLineIndent=-1, thisLineIndent=0, prevLineIndent=0;
   TokenType thisLineIndentType=INDENT, nextLineIndentType=INDENT;
-  for (list<Token>::iterator p = in.begin(); p != in.end(); ++p) {
-    cerr << *p;
-    if (isIndent(*p)) cerr << "(" << p->indentLevel << ") ";
-    cerr << " ";
-  }
-  cerr << endl;
 
   for (list<Token>::iterator p = in.begin(); p != in.end(); p = skipLine(p, in.end())) {
     list<Token> line = slurpLine(p, in.end());
@@ -582,12 +570,6 @@ list<Token> parenthesize(list<Token> in) {
     nextLineIndent = whitespace(line.back().type) ? line.back().indentLevel : 0;
     nextLineIndentType = line.back().type;
     bool insertedParenThisLine = false;
-
-    cerr << "===\n";
-    cerr << "AA: " << prevLineIndent << " " << thisLineIndent << " " << nextLineIndent << endl;
-    for (list<Token>::iterator q = line.begin(); q != line.end(); ++q)
-      cerr << *q << " ";
-    cerr << endl;
 
     if (!suppressInsert && numWords > 1
         && line.front() != L"(" && line.front() != L"'" && line.front() != L"`"
