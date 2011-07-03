e9e21c42f49de7e97145b6b3953d2521e12a1c62
Kartik K. Agaram <vc@akkartik.com>
2011-07-03 14:26:55 -0700

    361

--- comp.cc
+++ comp.cc
@@ -13,3 +13,16 @@ void test_cons_works() {
   rmref(call);
   checkState();
 }
+
+COMPILE_PRIM_FUNC(plus, (x y),
+  result = newNum(toNum(lookup(L"x"))+toNum(lookup(L"y")));
+)
+
+void test_plus_works() {
+  Cell* call = buildCells(parse(parenthesize(tokenize(teststream(L"plus 1 2"))))).front();
+  Cell* result = eval(call);
+  check_eq(toNum(result), 3);
+  rmref(result);
+  rmref(call);
+  checkState();
+}
