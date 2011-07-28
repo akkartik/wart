4033a3a4e6ccbc5e68ef244219b43dbeddb11475
Kartik K. Agaram <vc@akkartik.com>
2011-07-27 17:52:54 -0700

    531 - had omitted to mkref result of arithmetic primFuncs in 503

--- 011arith.cc
+++ 011arith.cc
@@ -1,19 +1,24 @@
 COMPILE_PRIM_FUNC(add, L"(x y)",
   result = newNum(toNum(lookup(L"x"))+toNum(lookup(L"y")));
+  mkref(result);
 )
 
 COMPILE_PRIM_FUNC(subtract, L"(x y)",
   result = newNum(toNum(lookup(L"x"))-toNum(lookup(L"y")));
+  mkref(result);
 )
 
 COMPILE_PRIM_FUNC(multiply, L"(x y)",
   result = newNum(toNum(lookup(L"x"))*toNum(lookup(L"y")));
+  mkref(result);
 )
 
 COMPILE_PRIM_FUNC(divide, L"(x y)",
   result = newNum(toNum(lookup(L"x"))/toNum(lookup(L"y")));
+  mkref(result);
 )
 
 COMPILE_PRIM_FUNC(modulo, L"(x y)",
   result = newNum(toNum(lookup(L"x"))%toNum(lookup(L"y")));
+  mkref(result);
 )
