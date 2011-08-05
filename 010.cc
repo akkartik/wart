df26f5678c1428075bc6428928cddedbe17e4dcf
Kartik K. Agaram <vc@akkartik.com>
2011-08-05 11:00:00 -0700

    566

--- 010.cc
+++ 010.cc
@@ -41,14 +41,14 @@ COMPILE_PRIM_FUNC(assign, L"('x y)",
   mkref(result);
 )
 
-// HACK because there's no wifstream(wstring) constructor
-// will only work with strings containing ascii characters
-vector<ascii> toAscii(string s) {
-  vector<ascii> result;
-  for (string::iterator p = s.begin(); p != s.end(); ++p)
-    result.push_back(*p);
-  return result;
-}
+                                  // HACK because there's no wifstream(wstring) constructor
+                                  // will only work with strings containing ascii characters
+                                  vector<ascii> toAscii(string s) {
+                                    vector<ascii> result;
+                                    for (string::iterator p = s.begin(); p != s.end(); ++p)
+                                      result.push_back(*p);
+                                    return result;
+                                  }
 
 COMPILE_PRIM_FUNC(load, L"(f)",
   loadFile(&toAscii(toString(lookup(L"f")))[0]);
