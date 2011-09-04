47baaa521e81e82d1aac3e62720a14603d8ce667
Kartik K. Agaram <vc@akkartik.com>
2011-09-03 22:56:26 -0700

    635

--- 013ssyntax.test.cc
+++ 013ssyntax.test.cc
@@ -1,8 +1,8 @@
 void test_ssyntax() {
-  SsyntaxTemplate s = {L'.', SsyntaxTemplate::IN_BETWEEN, newSym(L"foo")};
+  SsyntaxTemplate s = {L'.', SsyntaxTemplate::IN_BETWEEN, newSym(L"op")};
   ssyntaxTemplates.push_back(s);
   Cell* cons = wartRead(stream(L"a.b")).front();
-  check_eq(car(cons), newSym(L"foo"));
+  check_eq(car(cons), newSym(L"op"));
   check_eq(car(cdr(cons)), newSym(L"a"));
   check_eq(car(cdr(cdr(cons))), newSym(L"b"));
   check_eq(cdr(cdr(cdr(cons))), nil);
@@ -11,7 +11,7 @@ void test_ssyntax() {
 }
 
 void test_ssyntax_skips_floats() {
-  SsyntaxTemplate s = {L'.', SsyntaxTemplate::IN_BETWEEN, newSym(L"foo")};
+  SsyntaxTemplate s = {L'.', SsyntaxTemplate::IN_BETWEEN, newSym(L"op")};
   ssyntaxTemplates.push_back(s);
   Cell* val = wartRead(stream(L"2.4")).front();
   check(isSym(val)); // fix when we support floats
@@ -21,10 +21,10 @@ void test_ssyntax_skips_floats() {
 }
 
 void test_ssyntax_setup() {
-  Cell* def = wartRead(stream(L"ssyntax _._ (foo _ _)")).front();
+  Cell* def = wartRead(stream(L"ssyntax _._ (op _ _)")).front();
   eval(def); // needn't rmref; returns nil
   Cell* expr = wartRead(stream(L"a.b")).front();
-  check_eq(car(expr), newSym(L"foo"));
+  check_eq(car(expr), newSym(L"op"));
   check_eq(car(cdr(expr)), newSym(L"a"));
   check_eq(car(cdr(cdr(expr))), newSym(L"b"));
   check_eq(cdr(cdr(cdr(expr))), nil);
