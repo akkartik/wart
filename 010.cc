209494035e366ba63ec0f7b3c5ace223de5c6167
Kartik K. Agaram <vc@akkartik.com>
2011-10-11 10:22:40 -0700

    904 - table_get

--- 010.cc
+++ 010.cc
@@ -143,7 +143,15 @@ COMPILE_PRIM_FUNC(table_set, primFunc_table_set, L"($table $key $val)",
   Cell* val = lookup(L"$val");
   if (isTable(table))
     set(table, key, val);
+  else
+    warn << "can't set in a non-table: " << table << endl;
   return mkref(val);
+)
+
+COMPILE_PRIM_FUNC(table_get, primFunc_table_get, L"($table $key)",
+  Cell* table = lookup(L"$table");
+  Cell* key = lookup(L"$key");
+  return mkref(get(table, key));
 )
 
 COMPILE_PRIM_FUNC(type, primFunc_type, L"($x)",
