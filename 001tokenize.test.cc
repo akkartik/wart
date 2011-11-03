266791894c72464702d70f1cdbf29b1ee7ee9558
Kartik K. Agaram <vc@akkartik.com>
2011-11-02 20:17:38 -0700

    1155 - migrate tests to streaming tokenize

--- 001tokenize.test.cc
+++ 001tokenize.test.cc
@@ -4,236 +4,216 @@ void test_tokenize_handles_empty_input() {
 }
 
 void test_tokenize_always_starts_a_line_with_indent() {
-  list<Token> tokens = tokenize(stream(L"34"));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"34"); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"34"));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"34");
+  check(eof(c.fd));
 }
 
 void test_tokenize_handles_multiple_atoms() {
-  list<Token> tokens = tokenize(stream(L"34 abc"));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"34"); ++p;
-  checkEq(*p, L"abc"); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"34 abc"));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"34");
+  checkEq(nextToken(c), L"abc");
+  check(eof(c.fd));
 }
 
 void test_tokenize_handles_string_literal() {
-  list<Token> tokens = tokenize(stream(L"34 \"abc\""));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"34"); ++p;
-  checkEq(*p, L"\"abc\""); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"34 \"abc\""));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"34");
+  checkEq(nextToken(c), L"\"abc\"");
+  check(eof(c.fd));
 }
 
 void test_tokenize_handles_multiple_lines() {
-  list<Token> tokens = tokenize(stream(L"34\n\"abc\""));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"34"); ++p;
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"\"abc\""); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"34\n\"abc\""));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"34");
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"\"abc\"");
+  check(eof(c.fd));
 }
 
 void test_tokenize_handles_string_with_space() {
-  list<Token> tokens = tokenize(stream(L"34\n\"abc def\""));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"34"); ++p;
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"\"abc def\""); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"34\n\"abc def\""));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"34");
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"\"abc def\"");
+  check(eof(c.fd));
 }
 
 void test_tokenize_handles_string_with_escape() {
-  list<Token> tokens = tokenize(stream(L"34\n\"abc \\\"quote def\""));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"34"); ++p;
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"\"abc \\\"quote def\""); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"34\n\"abc \\\"quote def\""));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"34");
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"\"abc \\\"quote def\"");
+  check(eof(c.fd));
 }
 
 void test_tokenize_handles_quote_comma() {
-  list<Token> tokens = tokenize(stream(L"',35"));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"'"); ++p;
-  checkEq(*p, L","); ++p;
-  checkEq(*p, L"35"); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"',35"));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"'");
+  checkEq(nextToken(c), L",");
+  checkEq(nextToken(c), L"35");
+  check(eof(c.fd));
 }
 
 void test_tokenize_handles_quote_comma_paren() {
-  list<Token> tokens = tokenize(stream(L"(',)"));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"("); ++p;
-  checkEq(*p, L"'"); ++p;
-  checkEq(*p, L","); ++p;
-  checkEq(*p, L")"); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"(',)"));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"(");
+  checkEq(nextToken(c), L"'");
+  checkEq(nextToken(c), L",");
+  checkEq(nextToken(c), L")");
+  check(eof(c.fd));
 }
 
 void test_tokenize_doesnt_break_comma_right_after_ssyntax_char() {
-  list<Token> tokens = tokenize(stream(L"'a:,b"));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"'"); ++p;
-  checkEq(*p, L"a:,b"); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"'a:,b"));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"'");
+  checkEq(nextToken(c), L"a:,b");
+  check(eof(c.fd));
 }
 
 void test_tokenize_handles_splice_operators() {
-  list<Token> tokens = tokenize(stream(L"()',@ @, @b"));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"("); ++p;
-  checkEq(*p, L")"); ++p;
-  checkEq(*p, L"'"); ++p;
-  checkEq(*p, L",@"); ++p;
-  checkEq(*p, L"@"); ++p;
-  checkEq(*p, L","); ++p;
-  checkEq(*p, L"@"); ++p;
-  checkEq(*p, L"b"); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"()',@ @, @b"));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"(");
+  checkEq(nextToken(c), L")");
+  checkEq(nextToken(c), L"'");
+  checkEq(nextToken(c), L",@");
+  checkEq(nextToken(c), L"@");
+  checkEq(nextToken(c), L",");
+  checkEq(nextToken(c), L"@");
+  checkEq(nextToken(c), L"b");
+  check(eof(c.fd));
 }
 
 void test_tokenize_handles_comment() {
-  list<Token> tokens = tokenize(stream(L"()',@ ;abc def ghi"));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"("); ++p;
-  checkEq(*p, L")"); ++p;
-  checkEq(*p, L"'"); ++p;
-  checkEq(*p, L",@"); ++p;
+  CodeStream c(stream(L"()',@ ;abc def ghi"));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"(");
+  checkEq(nextToken(c), L")");
+  checkEq(nextToken(c), L"'");
+  checkEq(nextToken(c), L",@");
 }
 
 void test_tokenize_ends_comment_at_newline() {
-  list<Token> tokens = tokenize(stream(L";abc def ghi\nabc"));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"abc"); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L";abc def ghi\nabc"));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"abc");
+  check(eof(c.fd));
 }
 
 void test_tokenize_suppresses_comments() {
-  list<Token> tokens = tokenize(stream(L"abc\n;abc\ndef\nghi"));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"abc"); ++p;
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"def"); ++p;
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"ghi"); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"abc\n;abc\ndef\nghi"));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"abc");
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"def");
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"ghi");
+  check(eof(c.fd));
 }
 
 void test_tokenize_suppresses_comments2() {
-  list<Token> tokens = tokenize(stream(L"a b\n  c\n;abc\ndef\n  ghi\n\njkl"));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"a"); ++p;
-  checkEq(*p, L"b"); ++p;
-  checkEq(*p, Token::indent(2)); ++p;
-  checkEq(*p, L"c"); ++p;
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"def"); ++p;
-  checkEq(*p, Token::indent(2)); ++p;
-  checkEq(*p, L"ghi"); ++p;
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"jkl"); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"a b\n  c\n;abc\ndef\n  ghi\n\njkl"));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"a");
+  checkEq(nextToken(c), L"b");
+  checkEq(nextToken(c), Token::indent(2));
+  checkEq(nextToken(c), L"c");
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"def");
+  checkEq(nextToken(c), Token::indent(2));
+  checkEq(nextToken(c), L"ghi");
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"jkl");
+  check(eof(c.fd));
 }
 
 void test_tokenize_suppresses_trailing_whitespace() {
-  list<Token> tokens = tokenize(stream(L"a \nb\r\nc"));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"a"); ++p;
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"b"); ++p;
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"c"); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"a \nb\r\nc"));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"a");
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"b");
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"c");
+  check(eof(c.fd));
 }
 
 void test_tokenize_suppresses_repeated_newline() {
-  list<Token> tokens = tokenize(stream(L"34\n\n\"abc \\\"quote def\""));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"34"); ++p;
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"\"abc \\\"quote def\""); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"34\n\n\"abc \\\"quote def\""));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"34");
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"\"abc \\\"quote def\"");
+  check(eof(c.fd));
 }
 
 void test_tokenize_handles_indent_outdent() {
-  list<Token> tokens = tokenize(stream(L"abc def ghi\n\n    abc\n  def"));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"abc"); ++p;
-  checkEq(*p, L"def"); ++p;
-  checkEq(*p, L"ghi"); ++p;
-  checkEq(*p, Token::indent(4)); ++p;
-  checkEq(*p, L"abc"); ++p;
-  checkEq(*p, Token::indent(2)); ++p;
-  checkEq(*p, L"def"); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"abc def ghi\n\n    abc\n  def"));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"abc");
+  checkEq(nextToken(c), L"def");
+  checkEq(nextToken(c), L"ghi");
+  checkEq(nextToken(c), Token::indent(4));
+  checkEq(nextToken(c), L"abc");
+  checkEq(nextToken(c), Token::indent(2));
+  checkEq(nextToken(c), L"def");
+  check(eof(c.fd));
 }
 
 void test_tokenize_suppresses_whitespace_lines() {
-  list<Token> tokens = tokenize(stream(L"abc def ghi\n\n    \n  def"));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"abc"); ++p;
-  checkEq(*p, L"def"); ++p;
-  checkEq(*p, L"ghi"); ++p;
-  checkEq(*p, Token::indent(2)); ++p;
-  checkEq(*p, L"def"); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"abc def ghi\n\n    \n  def"));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"abc");
+  checkEq(nextToken(c), L"def");
+  checkEq(nextToken(c), L"ghi");
+  checkEq(nextToken(c), Token::indent(2));
+  checkEq(nextToken(c), L"def");
+  check(eof(c.fd));
 }
 
 void test_tokenize_suppresses_whitespace_lines2() {
-  list<Token> tokens = tokenize(stream(L"  \nabc def ghi\n\n    \n  def"));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"abc"); ++p;
-  checkEq(*p, L"def"); ++p;
-  checkEq(*p, L"ghi"); ++p;
-  checkEq(*p, Token::indent(2)); ++p;
-  checkEq(*p, L"def"); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"  \nabc def ghi\n\n    \n  def"));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"abc");
+  checkEq(nextToken(c), L"def");
+  checkEq(nextToken(c), L"ghi");
+  checkEq(nextToken(c), Token::indent(2));
+  checkEq(nextToken(c), L"def");
+  check(eof(c.fd));
 }
 
 void test_tokenize_handles_sexpr() {
-  list<Token> tokens = tokenize(stream(L"('a '(boo) \"foo\nbar\" `c `,d ,@e)\nabc ;def ghi\ndef"));
-  list<Token>::iterator p = tokens.begin();
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"("); ++p;
-  checkEq(*p, L"'"); ++p;
-  checkEq(*p, L"a"); ++p;
-  checkEq(*p, L"'"); ++p;
-  checkEq(*p, L"("); ++p;
-  checkEq(*p, L"boo"); ++p;
-  checkEq(*p, L")"); ++p;
-  checkEq(*p, L"\"foo\nbar\""); ++p;
-  checkEq(*p, L"`"); ++p;
-  checkEq(*p, L"c"); ++p;
-  checkEq(*p, L"`"); ++p;
-  checkEq(*p, L","); ++p;
-  checkEq(*p, L"d"); ++p;
-  checkEq(*p, L",@"); ++p;
-  checkEq(*p, L"e"); ++p;
-  checkEq(*p, L")"); ++p;
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"abc"); ++p;
-  checkEq(*p, Token::indent(0)); ++p;
-  checkEq(*p, L"def"); ++p;
-  check(p == tokens.end());
+  CodeStream c(stream(L"('a '(boo) \"foo\nbar\" `c `,d ,@e)\nabc ;def ghi\ndef"));
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"(");
+  checkEq(nextToken(c), L"'");
+  checkEq(nextToken(c), L"a");
+  checkEq(nextToken(c), L"'");
+  checkEq(nextToken(c), L"(");
+  checkEq(nextToken(c), L"boo");
+  checkEq(nextToken(c), L")");
+  checkEq(nextToken(c), L"\"foo\nbar\"");
+  checkEq(nextToken(c), L"`");
+  checkEq(nextToken(c), L"c");
+  checkEq(nextToken(c), L"`");
+  checkEq(nextToken(c), L",");
+  checkEq(nextToken(c), L"d");
+  checkEq(nextToken(c), L",@");
+  checkEq(nextToken(c), L"e");
+  checkEq(nextToken(c), L")");
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"abc");
+  checkEq(nextToken(c), Token::indent(0));
+  checkEq(nextToken(c), L"def");
+  check(eof(c.fd));
 }
