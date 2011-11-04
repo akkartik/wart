ce843d6f2470850c4d20ea69d446c4aa35f406a6
Kartik K. Agaram <vc@akkartik.com>
2011-11-04 14:40:56 -0700

    1200 - sleep

--- 017posix.cc
+++ 017posix.cc
@@ -6,3 +6,8 @@ COMPILE_PRIM_FUNC(wait_for_child, primFunc_wait_for_child, L"()",
   wait(NULL);
   return nil;
 )
+
+COMPILE_PRIM_FUNC(sleep, primFunc_sleep, L"($n)",
+  sleep(toNum(lookup(L"$n")));
+  return nil;
+)
