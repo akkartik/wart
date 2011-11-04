771c2d448ce9f3f2bbb91b532aed980106e57e94
Kartik K. Agaram <vc@akkartik.com>
2011-11-04 11:26:59 -0700

    1197 - wait() to harvest zombies

--- 017posix.cc
+++ 017posix.cc
@@ -1,3 +1,8 @@
 COMPILE_PRIM_FUNC(fork, primFunc_fork, L"()",
   return mkref(newNum(fork()));
 )
+
+COMPILE_PRIM_FUNC(wait_for_child, primFunc_wait_for_child, L"()",
+  wait(NULL);
+  return nil;
+)
