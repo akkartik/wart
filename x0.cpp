ccce000e40d32d2cea3ee933169db5a9696cfe59
Kartik K. Agaram <vc@akkartik.com>
2011-11-24 12:53:21 -0800

    1300 - put both versions in standalone binaries; no errors in either

---
+++ x0.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<netdb.h>
+
+#define PERR(call...) if (call < 0) perror(#call)
+
+char buf[BUFSIZ];
+socklen_t n;
+
+int main() {
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) perror("socket() failed");
+  int dummy;
+  PERR(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &dummy, sizeof(dummy)));
+  sockaddr_in s;  s.sin_family = AF_INET;   s.sin_addr.s_addr = INADDR_ANY;
+  s.sin_port = htons(4040);
+  PERR(bind(sockfd, (sockaddr*)&s, sizeof(s)));
+  PERR(listen(sockfd, 5));
+
+  sockaddr_in t;  n = sizeof(sockaddr_in);
+  int clientsockfd = accept(sockfd, (sockaddr*)&t, &n);
+
+  read(clientsockfd, buf, BUFSIZ-1);
+
+  close(clientsockfd);
+  close(sockfd);
+  return 0;
+}
