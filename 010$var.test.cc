bd21add690c99e09df988f009e7eec9d253121eb
Kartik K. Agaram <vc@akkartik.com>
2012-02-15 13:07:47 -0800

    1531 - temporarily clear eval's users

--- 010$var.test.cc
+++
@@ -1,6 +0,0 @@
-void test_transform_handles_dollar_vars() {
-  Cell* expr = read(stream("$x"));
-  check(isSym(expr));
-  checkEq(toString(expr).substr(0, 1), "x");
-  rmref(expr);
-}
--- 014num.test.cc
+++
@@ -1,7 +0,0 @@
-void test_add_works() {
-  Cell* call = read(stream("+ 1 2"));
-  Cell* result = eval(call);
-  checkEq(toNum(result), 3);
-  rmref(result);
-  rmref(call);
-}
