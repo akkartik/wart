6e885a2d4fc857e917f92bd1b2d984deb688c521
Kartik K. Agaram <vc@akkartik.com>
2012-05-06 01:41:51 -0700

    1700 - arithmetic for floats

--- 014num.cc
+++ 014num.cc
@@ -1,23 +1,39 @@
 COMPILE_FN(+, compiledFn_add, "($x $y)",
-  return mkref(newNum(toInt(lookup("$x")) + toInt(lookup("$y"))));
+  Cell* x = lookup("$x"); Cell* y = lookup("$y");
+  if (x->type == FLOAT || y->type == FLOAT)
+    return mkref(newNum(toFloat(x) + toFloat(y)));
+  else
+    return mkref(newNum(toInt(x) + toInt(y)));
 )
 
 COMPILE_FN(-, compiledFn_subtract, "($x $y)",
-  return mkref(newNum(toInt(lookup("$x")) - toInt(lookup("$y"))));
+  Cell* x = lookup("$x"); Cell* y = lookup("$y");
+  if (x->type == FLOAT || y->type == FLOAT)
+    return mkref(newNum(toFloat(x) - toFloat(y)));
+  else
+    return mkref(newNum(toInt(x) - toInt(y)));
 )
 
 COMPILE_FN(*, compiledFn_multiply, "($x $y)",
-  return mkref(newNum(toInt(lookup("$x")) * toInt(lookup("$y"))));
+  Cell* x = lookup("$x"); Cell* y = lookup("$y");
+  if (x->type == FLOAT || y->type == FLOAT)
+    return mkref(newNum(toFloat(x) * toFloat(y)));
+  else
+    return mkref(newNum(toInt(x) * toInt(y)));
 )
 
 COMPILE_FN(/, compiledFn_divide, "($x $y)",
-  return mkref(newNum(toInt(lookup("$x")) / toInt(lookup("$y"))));
+  Cell* x = lookup("$x"); Cell* y = lookup("$y");
+  if (x->type == FLOAT || y->type == FLOAT)
+    return mkref(newNum(toFloat(x) / toFloat(y)));
+  else
+    return mkref(newNum(toInt(x) / toInt(y)));
 )
 
 COMPILE_FN(%, compiledFn_modulo, "($x $y)",
-  return mkref(newNum(toInt(lookup("$x")) % toInt(lookup("$y"))));
+  return mkref(newNum(toInt(lookup("$x")) % toInt(lookup("$y")))); // what does modulo of floats mean?
 )
 
 COMPILE_FN(<, compiledFn_lesser, "($x $y)",
-  return toInt(lookup("$x")) < toInt(lookup("$y")) ? mkref(lookup("$x")) : nil;
+  return toFloat(lookup("$x")) < toFloat(lookup("$y")) ? mkref(lookup("$x")) : nil;
 )
--- 014num.test.cc
+++ 014num.test.cc
@@ -5,3 +5,11 @@ void test_add_works() {
   rmref(result);
   rmref(call);
 }
+
+void test_add_works_for_floats() {
+  Cell* call = read(stream("+ 1.0 2.0"));
+  Cell* result = eval(call);
+  checkEq(toFloat(result), 3.0);
+  rmref(result);
+  rmref(call);
+}
