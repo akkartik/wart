3e9c640d87d05b6d54f194e4171ec472b3dd90bd
Kartik K. Agaram <vc@akkartik.com>
2012-08-06 23:56:11 -0700

    1954

--- 002parenthesize.cc
+++ 002parenthesize.cc
@@ -1,60 +1,5 @@
 //// insert explicit parens based on indentation
 
-                                  bool endOfInput(istream& in) {
-                                    if (in.eof()) return true;
-                                    if (!interactive) return false;
-                                    // in interactive mode two <Enter>s are like eof
-                                    bool ans = false;
-                                    char c = in.get();
-                                    if (c == '\n' && !in.eof() && in.peek() == '\n')
-                                      ans = true;
-                                    in.putback(c);
-                                    return ans;
-                                  }
-
-list<Token> nextLine(CodeStream& c) {
-  list<Token> result;
-  if (endOfInput(c.fd)) return result;
-
-  if (c.currIndent == -1)
-    result.push_back(Token(c.currIndent=indent(c.fd)));
-  else
-    result.push_back(Token(c.currIndent));
-
-  do { result.push_back(nextToken(c)); }
-  while (!endOfInput(c.fd) && !result.back().isIndent());
-  return result;
-}
-
-
-
-                                  long numWordsInLine(list<Token> line) {
-                                    long numWords = 0;
-                                    for (list<Token>::iterator p = line.begin(); p != line.end(); ++p)
-                                      if (!p->isIndent() && !p->isParen()
-                                          && !p->isQuoteOrUnquote())
-                                        ++numWords;
-                                    return numWords;
-                                  }
-
-                                  void add(list<Token>& l, Token x) {
-                                    if (!x.isIndent())
-                                      l.push_back(x);
-                                  }
-
-                                  list<Token>::iterator firstNonQuote(list<Token>& line) {
-                                    for (list<Token>::iterator p = line.begin(); p != line.end(); ++p) {
-                                      if (!p->isIndent() && !p->isQuoteOrUnquote())
-                                        return p;
-                                    }
-                                    return line.end();
-                                  }
-
-                                  bool noParenAtStart(list<Token> line) {
-                                    list<Token>::iterator p = firstNonQuote(line);
-                                    return p != line.end() && *p != "(";
-                                  }
-
 list<Token> nextExpr(CodeStream& c) {
   list<Token> result;
   stack<long> explicitParenStack; // parens in the original
@@ -99,4 +44,61 @@ list<Token> nextExpr(CodeStream& c) {
   for (unsigned long i=0; i < implicitParenStack.size(); ++i)
     result.push_back(Token(")"));
   return result;
+}
+
+
+
+// internals
+
+list<Token> nextLine(CodeStream& c) {
+  list<Token> result;
+  if (endOfInput(c.fd)) return result;
+
+  if (c.currIndent == -1)
+    result.push_back(Token(c.currIndent=indent(c.fd)));
+  else
+    result.push_back(Token(c.currIndent));
+
+  do { result.push_back(nextToken(c)); }
+  while (!endOfInput(c.fd) && !result.back().isIndent());
+  return result;
+}
+
+bool endOfInput(istream& in) {
+  if (in.eof()) return true;
+  if (!interactive) return false;
+  // in interactive mode two <Enter>s are like eof
+  bool ans = false;
+  char c = in.get();
+  if (c == '\n' && !in.eof() && in.peek() == '\n')
+    ans = true;
+  in.putback(c);
+  return ans;
+}
+
+long numWordsInLine(list<Token> line) {
+  long numWords = 0;
+  for (list<Token>::iterator p = line.begin(); p != line.end(); ++p)
+    if (!p->isIndent() && !p->isParen()
+        && !p->isQuoteOrUnquote())
+      ++numWords;
+  return numWords;
+}
+
+void add(list<Token>& l, Token x) {
+  if (!x.isIndent())
+    l.push_back(x);
+}
+
+list<Token>::iterator firstNonQuote(list<Token>& line) {
+  for (list<Token>::iterator p = line.begin(); p != line.end(); ++p) {
+    if (!p->isIndent() && !p->isQuoteOrUnquote())
+      return p;
+  }
+  return line.end();
+}
+
+bool noParenAtStart(list<Token> line) {
+  list<Token>::iterator p = firstNonQuote(line);
+  return p != line.end() && *p != "(";
 }
