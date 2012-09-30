bf1fa1b899d1f38ecdcbe2bd62cda3e1ded026ed
Kartik K. Agaram <vc@akkartik.com>
2012-09-30 16:20:25 -0700

    1999 - fix old double-eval in tokenizer tests

--- 001tokenize.test.cc
+++ 001tokenize.test.cc
@@ -2,197 +2,217 @@
 
 void test_tokenize_always_starts_a_line_with_indent() {
   CodeStream c(stream("34"));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "34");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));       t=nextToken(c);
+  checkEq(t, "34");            t=nextToken(c);
 }
 
 void test_tokenize_handles_multiple_atoms() {
   CodeStream c(stream("34 abc 3.4"));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "34");
-  checkEq(nextToken(c), "abc");
-  checkEq(nextToken(c), "3.4");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));       t=nextToken(c);
+  checkEq(t, "34");            t=nextToken(c);
+  checkEq(t, "abc");           t=nextToken(c);
+  checkEq(t, "3.4");           t=nextToken(c);
 }
 
 void test_tokenize_handles_string_literal() {
   CodeStream c(stream("34 \"abc\""));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "34");
-  checkEq(nextToken(c), "\"abc\"");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));       t=nextToken(c);
+  checkEq(t, "34");            t=nextToken(c);
+  checkEq(t, "\"abc\"");       t=nextToken(c);
 }
 
 void test_tokenize_handles_multiple_lines() {
   CodeStream c(stream("34\n\"abc\""));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "34");
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "\"abc\"");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));       t=nextToken(c);
+  checkEq(t, "34");            t=nextToken(c);
+  checkEq(t, indent(0));       t=nextToken(c);
+  checkEq(t, "\"abc\"");       t=nextToken(c);
 }
 
 void test_tokenize_handles_string_with_space() {
   CodeStream c(stream("34\n\"abc def\""));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "34");
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "\"abc def\"");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));       t=nextToken(c);
+  checkEq(t, "34");            t=nextToken(c);
+  checkEq(t, indent(0));       t=nextToken(c);
+  checkEq(t, "\"abc def\"");   t=nextToken(c);
 }
 
 void test_tokenize_handles_string_with_escape() {
   CodeStream c(stream("34\n\"abc \\\"quote def\""));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "34");
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "\"abc \\\"quote def\"");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));       t=nextToken(c);
+  checkEq(t, "34");            t=nextToken(c);
+  checkEq(t, indent(0));       t=nextToken(c);
+  checkEq(t, "\"abc \\\"quote def\"");
 }
 
 void test_tokenize_handles_quote_comma() {
   CodeStream c(stream("',35"));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "'");
-  checkEq(nextToken(c), ",");
-  checkEq(nextToken(c), "35");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "'");            t=nextToken(c);
+  checkEq(t, ",");            t=nextToken(c);
+  checkEq(t, "35");           t=nextToken(c);
 }
 
 void test_tokenize_handles_quote_comma_paren() {
   CodeStream c(stream("(',)"));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "(");
-  checkEq(nextToken(c), "'");
-  checkEq(nextToken(c), ",");
-  checkEq(nextToken(c), ")");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "(");            t=nextToken(c);
+  checkEq(t, "'");            t=nextToken(c);
+  checkEq(t, ",");            t=nextToken(c);
+  checkEq(t, ")");            t=nextToken(c);
 }
 
 void test_tokenize_doesnt_break_comma_right_after_ssyntax_char() {
   CodeStream c(stream("'a:,b"));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "'");
-  checkEq(nextToken(c), "a:,b");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "'");            t=nextToken(c);
+  checkEq(t, "a:,b");         t=nextToken(c);
 }
 
 void test_tokenize_handles_splice_operators() {
   CodeStream c(stream("()',@ @, @b"));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "(");
-  checkEq(nextToken(c), ")");
-  checkEq(nextToken(c), "'");
-  checkEq(nextToken(c), ",@");
-  checkEq(nextToken(c), "@");
-  checkEq(nextToken(c), ",");
-  checkEq(nextToken(c), "@");
-  checkEq(nextToken(c), "b");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "(");            t=nextToken(c);
+  checkEq(t, ")");            t=nextToken(c);
+  checkEq(t, "'");            t=nextToken(c);
+  checkEq(t, ",@");           t=nextToken(c);
+  checkEq(t, "@");            t=nextToken(c);
+  checkEq(t, ",");            t=nextToken(c);
+  checkEq(t, "@");            t=nextToken(c);
+  checkEq(t, "b");            t=nextToken(c);
 }
 
 void test_tokenize_handles_comment() {
   CodeStream c(stream("()',@ ;abc def ghi"));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "(");
-  checkEq(nextToken(c), ")");
-  checkEq(nextToken(c), "'");
-  checkEq(nextToken(c), ",@");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "(");            t=nextToken(c);
+  checkEq(t, ")");            t=nextToken(c);
+  checkEq(t, "'");            t=nextToken(c);
+  checkEq(t, ",@");           t=nextToken(c);
 }
 
 void test_tokenize_ends_comment_at_newline() {
   CodeStream c(stream(";abc def ghi\nabc"));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "abc");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "abc");          t=nextToken(c);
 }
 
 void test_tokenize_suppresses_comments() {
   CodeStream c(stream("abc\n;abc\ndef\nghi"));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "abc");
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "def");
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "ghi");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "abc");          t=nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "def");          t=nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "ghi");          t=nextToken(c);
 }
 
 void test_tokenize_suppresses_comments2() {
   CodeStream c(stream("a : b\n  : c\n;abc\ndef :\n  ghi\n\njkl"));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "a");
-  checkEq(nextToken(c), "b");
-  checkEq(nextToken(c), indent(2));
-  checkEq(nextToken(c), "c");
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "def");
-  checkEq(nextToken(c), indent(2));
-  checkEq(nextToken(c), "ghi");
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "jkl");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "a");            t=nextToken(c);
+  checkEq(t, "b");            t=nextToken(c);
+  checkEq(t, indent(2));      t=nextToken(c);
+  checkEq(t, "c");            t=nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "def");          t=nextToken(c);
+  checkEq(t, indent(2));      t=nextToken(c);
+  checkEq(t, "ghi");          t=nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "jkl");          t=nextToken(c);
 }
 
 void test_tokenize_suppresses_trailing_whitespace() {
   CodeStream c(stream("a \nb\r\nc"));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "a");
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "b");
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "c");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "a");            t=nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "b");            t=nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "c");            t=nextToken(c);
 }
 
 void test_tokenize_suppresses_repeated_newline() {
   CodeStream c(stream("34\n\n\"abc \\\"quote def\""));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "34");
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "\"abc \\\"quote def\"");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "34");           t=nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "\"abc \\\"quote def\"");
 }
 
 void test_tokenize_handles_indent_outdent() {
   CodeStream c(stream("abc def ghi\n\n    abc\n  def"));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "abc");
-  checkEq(nextToken(c), "def");
-  checkEq(nextToken(c), "ghi");
-  checkEq(nextToken(c), indent(4));
-  checkEq(nextToken(c), "abc");
-  checkEq(nextToken(c), indent(2));
-  checkEq(nextToken(c), "def");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "abc");          t=nextToken(c);
+  checkEq(t, "def");          t=nextToken(c);
+  checkEq(t, "ghi");          t=nextToken(c);
+  checkEq(t, indent(4));      t=nextToken(c);
+  checkEq(t, "abc");          t=nextToken(c);
+  checkEq(t, indent(2));      t=nextToken(c);
+  checkEq(t, "def");          t=nextToken(c);
 }
 
 void test_tokenize_suppresses_whitespace_lines() {
   CodeStream c(stream("abc def ghi\n\n    \n  def"));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "abc");
-  checkEq(nextToken(c), "def");
-  checkEq(nextToken(c), "ghi");
-  checkEq(nextToken(c), indent(2));
-  checkEq(nextToken(c), "def");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "abc");          t=nextToken(c);
+  checkEq(t, "def");          t=nextToken(c);
+  checkEq(t, "ghi");          t=nextToken(c);
+  checkEq(t, indent(2));      t=nextToken(c);
+  checkEq(t, "def");          t=nextToken(c);
 }
 
 void test_tokenize_suppresses_whitespace_lines2() {
   CodeStream c(stream("  \nabc def ghi\n\n    \n  def"));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "abc");
-  checkEq(nextToken(c), "def");
-  checkEq(nextToken(c), "ghi");
-  checkEq(nextToken(c), indent(2));
-  checkEq(nextToken(c), "def");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "abc");          t=nextToken(c);
+  checkEq(t, "def");          t=nextToken(c);
+  checkEq(t, "ghi");          t=nextToken(c);
+  checkEq(t, indent(2));      t=nextToken(c);
+  checkEq(t, "def");          t=nextToken(c);
 }
 
 void test_tokenize_handles_sexpr() {
   CodeStream c(stream("('a '(boo) \"foo\nbar\" `c `,d ,@e)\nabc ;def ghi\ndef"));
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "(");
-  checkEq(nextToken(c), "'");
-  checkEq(nextToken(c), "a");
-  checkEq(nextToken(c), "'");
-  checkEq(nextToken(c), "(");
-  checkEq(nextToken(c), "boo");
-  checkEq(nextToken(c), ")");
-  checkEq(nextToken(c), "\"foo\nbar\"");
-  checkEq(nextToken(c), "`");
-  checkEq(nextToken(c), "c");
-  checkEq(nextToken(c), "`");
-  checkEq(nextToken(c), ",");
-  checkEq(nextToken(c), "d");
-  checkEq(nextToken(c), ",@");
-  checkEq(nextToken(c), "e");
-  checkEq(nextToken(c), ")");
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "abc");
-  checkEq(nextToken(c), indent(0));
-  checkEq(nextToken(c), "def");
+  Token t = nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "(");            t=nextToken(c);
+  checkEq(t, "'");            t=nextToken(c);
+  checkEq(t, "a");            t=nextToken(c);
+  checkEq(t, "'");            t=nextToken(c);
+  checkEq(t, "(");            t=nextToken(c);
+  checkEq(t, "boo");          t=nextToken(c);
+  checkEq(t, ")");            t=nextToken(c);
+  checkEq(t, "\"foo\nbar\""); t=nextToken(c);
+  checkEq(t, "`");            t=nextToken(c);
+  checkEq(t, "c");            t=nextToken(c);
+  checkEq(t, "`");            t=nextToken(c);
+  checkEq(t, ",");            t=nextToken(c);
+  checkEq(t, "d");            t=nextToken(c);
+  checkEq(t, ",@");           t=nextToken(c);
+  checkEq(t, "e");            t=nextToken(c);
+  checkEq(t, ")");            t=nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "abc");          t=nextToken(c);
+  checkEq(t, indent(0));      t=nextToken(c);
+  checkEq(t, "def");          t=nextToken(c);
 }
