d4d0de8aa5ec1fa0ee9875df2d3cdf6ae9f38735
Kartik K. Agaram <vc@akkartik.com>
2012-10-03 11:24:17 -0700

    2034

--- 010main.cc
+++ 010main.cc
@@ -52,6 +52,8 @@ const TestFn tests[] = {
 void runTests() {
   runningTests = true;
   pretendRaise = true;  // for death tests
+  time_t t; time(&t);
+  cerr << "-- C tests: " << ctime(&t);
   for (unsigned long i=0; i < sizeof(tests)/sizeof(tests[0]); ++i) {
     setup();
     (*tests[i])();
