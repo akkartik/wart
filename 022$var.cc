c5e5b098848cf1a0ff7a70ddf7898e36825f9775
Kartik K. Agaram <vc@akkartik.com>
2012-10-08 16:27:26 -0700

    2126 - design summary for $vars

--- 022$var.cc
+++ 022$var.cc
@@ -1,4 +1,41 @@
-//// implicit gensyms: $vars turn into unique syms before they're eval'd
+//// implicit gensyms: read $vars as unique syms
+
+// Variables defined in macros can cause subtle bugs:
+//   mac bad-swap(x y)
+//     `(let tmp ,x             ; tmp can be captured
+//        (,x = ,y)
+//        (,y = tmp))
+//
+//   wart> (withs (a 3 b 4)    (bad-swap a b)    (list a b))
+//   (4 3)   ; seems ok
+//
+//   wart> (withs (a 3 tmp 4)  (bad-swap a tmp)  (list a tmp))
+//   (3 4)   ; oops
+//
+// To avoid such bugs, use an implicit gensym:
+//   mac good-swap(x y)
+//     `(let $tmp ,x
+//        (,x = ,y)
+//        (,y = $tmp))
+//
+// When reading this definition, occurrences of $tmp are replaced with a
+// unique symbol like tmp2417. Occurrences of $tmp elsewhere will get a
+// *different* unique symbol.
+
+// Design considered the following:
+//  simple implementation
+//    so uniqueness isn't perfectly guaranteed, just overwhelmingly likely
+//  $var expansion is separate from tokenization
+//  runs after infix transformation, so $+ => +147 isn't turned into '(+ 147)
+//  don't expand $vars in compiled primitives
+
+// Design alternative: older lisps use explicit gensyms, which make for less
+// concise macros:
+//   mac old-swap(x y)
+//     let tmp (uniq 'tmp)      ; uniq was called gensym in older lisps
+//       `(let ,tmp ,x
+//          (,x = ,y)
+//          (,y = ,tmp))
 
 Cell* transformDollarVars(Cell* input) {
   Table map;  // transform $vars identically within each top-level expression
@@ -8,7 +45,7 @@ Cell* transformDollarVars(Cell* input) {
 Cell* transformDollarVars(Cell* input, Table& map) {
   if (isSym(input) && toString(input)[0] == '$') {
     if (!map[input])
-      map[mkref(input)] = mkref(genSym(newSym(toString(input).substr(1))));   // against destruction of map
+      map[mkref(input)] = mkref(genSym(newSym(toString(input).substr(1))));
     return map[input];
   }
 
