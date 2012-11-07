dceb214705b6e1aaa7f074a11a96c5820ba11056
Kartik K. Agaram <vc@akkartik.com>
2012-11-07 11:05:37 -0800

    2365

--- 021eval.cc
+++ 021eval.cc
@@ -11,6 +11,7 @@
 //  varargs functions: ((fn params params) 1 2 3) => (1 2 3)
 //  varargs functions with some named params: ((fn (fmt ... rest) (printf fmt rest)) "%d%d" 34 35) => "3435"
 //  passing in lists to functions: ((fn ((x y)) (+ x y)) '(3 4)) => 7
+//  naming list args and their parts: ((fn (l | (head ... tail)) (prn l " starts with " head)) '(1 2 3))
 //  functions can reorder args using keyords: ((fn (a b c) b) :b 3 1 2) => 3
 //  functions can document keyword args with a different name: ((fn (a b|returning c) b) :returning 3 1 2) => 3
 //  callers can pass in keyword args to nested functions
