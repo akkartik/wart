d871746d7a6505062c23413fa9d2c6cc24a7b867
Kartik K. Agaram <vc@akkartik.com>
2012-12-01 03:20:55 -0800

    2415

--- alternatives/batch/024string.test.cc
+++ alternatives/batch/024string.test.cc
@@ -13,3 +13,13 @@ void test_string_compare() {
   rmref(result);
   rmref(expr);
 }
+
+void test_string_split() {
+  Cell* expr = read("(split \"abc abd\")");
+  Cell* result = eval(expr);
+  checkEq(toString(car(result)), "abc");
+  checkEq(toString(car(cdr(result))), "abd");
+  checkEq(cdr(cdr(result)), nil);
+  rmref(result);
+  rmref(expr);
+}
