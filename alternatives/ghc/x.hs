f51a682e06d5152fbf6a25e7ab2d1946765ba8f3
Kartik K. Agaram <vc@akkartik.com>
2012-12-09 12:33:14 -0800

    2441 - back to the old low-level impl

--- alternatives/ghc/x.hs
+++ alternatives/ghc/x.hs
@@ -11,9 +11,41 @@ main =
                          print e
             Right r -> mapM_ print r
 
-csvFile = sepEndBy line eol
-line = sepBy cell (char ',')
-cell = quotedCell <|> many (noneOf ",\n\r")
+{- A CSV file contains 0 or more lines, each of which is terminated
+   by the end-of-line character (eol). -}
+csvFile :: GenParser Char st [[String]]
+csvFile =
+    do result <- many line
+       eof
+       return result
+
+-- Each line contains 1 or more cells, separated by a comma
+line :: GenParser Char st [String]
+line =
+    do result <- cells
+       eol                       -- end of line
+       return result
+
+-- Build up a list of cells.  Try to parse the first cell, then figure out
+-- what ends the cell.
+cells :: GenParser Char st [String]
+cells =
+    do first <- quotedCell <|> cellContent
+       next <- remainingCells
+       return (first : next)
+
+-- The cell either ends with a comma, indicating that 1 or more cells follow,
+-- or it doesn't, indicating that we're at the end of the cells for this line
+remainingCells :: GenParser Char st [String]
+remainingCells =
+    (char ',' >> cells)            -- Found comma?  More cells coming
+    <|> (return [])                -- No comma?  Return [], no more cells
+
+-- Each cell contains 0 or more characters, which must not be a comma or
+-- EOL
+cellContent :: GenParser Char st String
+cellContent =
+    many (noneOf ",\n")
 
 quotedCell =
     do char '"'
@@ -38,11 +70,11 @@ test1 = ParsecTest {
   parser = csvFile
 , initState = ()
 , positiveCases = [
-      ([["hi"]], ["hi"])
-    , ([["hi", "there"]], ["hi,there"])
-    , ([["hi"], [""], ["there"]], ["hi\n\nthere"])
-    , ([["a, b"]], ["\"a, b\""])
-    , ([["a, \"b"]], ["\"a, \"\"b\""])
+      ([["hi"]], ["hi\n"])
+    , ([["hi", "there"]], ["hi,there\n"])
+    , ([["hi"], [""], ["there"]], ["hi\n\nthere\n"])
+    , ([["a, b"]], ["\"a, b\"\n"])
+    , ([["a, \"b"]], ["\"a, \"\"b\"\n"])
     ]
 , negativeCases = []
 }
