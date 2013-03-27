52a16944c2a52d0ea87c47b6e3613eff6685d597
Kartik K. Agaram <vc@akkartik.com>
2013-03-27 12:41:36 -0700

    2644 - alternative story for quoted params

--- 021eval.cc
+++ 021eval.cc
@@ -95,27 +95,34 @@ Cell* eval(Cell* expr, Cell* scope) {
 //  aliased params
 void evalBindAll(Cell* params, Cell* args, Cell* scope, Cell* newScope) {
   if (params == nil)
-    ;
+    return;
 
-  else if (isSym(stripQuote(params))) {
-    Cell* dummy = NULL;
-    evalBindRest(params, args, &dummy, scope, newScope);
+  Cell* args2 = NULL;
+  if (isQuoted(params)) {
+    params = stripQuote(params);
+    args2 = quoteAll(args);
+  }
+  else {
+    args2 = mkref(args);
   }
 
-  else if (!isCons(stripQuote(params)))
+  if (isSym(params)) {
+    Cell* dummy = NULL;
+    evalBindRest(params, args2, &dummy, scope, newScope);
+  }
+
+  else if (!isCons(params))
     ;
 
-  else if (isAlias(stripQuote(params)))
-    evalBindRestAliases(params, args, scope, newScope);
-
-  else if (isQuoted(params))
-    bindParams(stripQuote(params), args, NULL, newScope);
+  else if (isAlias(params))
+    evalBindRestAliases(params, args2, scope, newScope);
 
   else {
     Cell* dummy = NULL;
-    evalBindParam(car(params), car(args), &dummy, scope, newScope);
-    evalBindAll(cdr(params), cdr(args), scope, newScope);
+    evalBindParam(car(params), car(args2), &dummy, scope, newScope);
+    evalBindAll(cdr(params), cdr(args2), scope, newScope);
   }
+  rmref(args2);
 }
 
 void evalBindRest(Cell* param, Cell* args, Cell** cachedVal, Cell* scope, Cell* newScope) {
@@ -647,4 +654,12 @@ Cell* env(Cell* fn) {
 
 bool isAlias(Cell* l) {
   return isCons(l) && car(l) == sym_param_alias;
+}
+
+Cell* quoteAll(Cell* x) {
+  Cell* result = newCell(), *curr = result;
+  for (Cell* iter = x; iter != nil; iter=cdr(iter), curr=cdr(curr))
+    addCons(curr, newCons(sym_quote, car(iter)));
+  result = dropPtr(result);
+  return result;
 }
