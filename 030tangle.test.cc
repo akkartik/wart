dbb301aca1385ed0c1b290fbf59fcdbf781e80b2
Kartik K. Agaram <vc@akkartik.com>
2013-05-04 10:06:20 -0700

    2729

--- 030tangle.test.cc
+++ 030tangle.test.cc
@@ -1,13 +1,3 @@
-void test_lines_works() {
-  std::istringstream in("a\nb\nc\n");
-  list<string> l = lines(in);
-  checkEq(l.front(), "a");  l.pop_front();
-  checkEq(l.front(), "b");  l.pop_front();
-  checkEq(l.front(), "c");  l.pop_front();
-  checkEq(l.front(), "");   l.pop_front();
-  check(l.empty());
-}
-
 void test_hunks_works_without_metadata() {
   std::istringstream in("a\nb\nc\n");
   list<string> l = hunks(in).front().lines;
