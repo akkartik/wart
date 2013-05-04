deec086ba2bcf57e29cd1d7a645f26ff68f7b0d8
Kartik K. Agaram <vc@akkartik.com>
2013-05-04 10:10:22 -0700

    2730

--- 030tangle.test.cc
+++ 030tangle.test.cc
@@ -7,3 +7,18 @@ void test_hunks_works_without_metadata() {
   checkEq(l.front(), "");   l.pop_front();
   check(l.empty());
 }
+
+void test_hunks_works_with_metadata() {
+  std::istringstream in("a\n:(a b)\nc\n");
+  list<Hunk> h = hunks(in);
+  list<string> l = h.front().lines;   h.pop_front();
+    checkEq(l.front(), "a");  l.pop_front();
+    checkEq(l.front(), "");   l.pop_front();
+    checkEq(l.empty());
+  l = h.front().lines;    h.pop_front();
+    checkEq(l.front(), ":(a b)");  l.pop_front();
+    checkEq(l.front(), "c");  l.pop_front();
+    checkEq(l.front(), "");   l.pop_front();
+    check(l.empty());
+  checkEq(h.empty());
+}
