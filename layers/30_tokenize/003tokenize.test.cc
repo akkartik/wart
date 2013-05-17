db47cb381eb286dcde84d0f768a3a000a389f33a
Kartik K. Agaram <vc@akkartik.com>
2013-05-16 19:52:25 -0700

    2776 - now all tokenize tests are tracified

--- layers/30_tokenize/003tokenize.test.cc
+++ layers/30_tokenize/003tokenize.test.cc
@@ -18,81 +18,60 @@ void test_tokenize_handles_multiple_lines() {
 
 void test_tokenize_handles_string_with_space() {
   stringstream in("34\n\"abc def\"");
-  Token t = nextToken(in);
-  CHECK_EQ(t, "34");            t=nextToken(in);
-  CHECK_EQ(t, "\"abc def\"");   t=nextToken(in);
+  readAll(in);
+  checkTraceContents("tokenize", "34\n\"abc def\"\n");
 }
 
 void test_tokenize_handles_string_with_escape() {
   stringstream in("34\n\"abc \\\"quote def\"");
-  Token t = nextToken(in);
-  CHECK_EQ(t, "34");            t=nextToken(in);
-  CHECK_EQ(t, "\"abc \\\"quote def\"");
+  readAll(in);
+  checkTraceContents("tokenize", "34\n\"abc \\\"quote def\"\n");
 }
 
 void test_tokenize_handles_comment() {
   stringstream in("()'a #abc def ghi");
-  Token t = nextToken(in);
-  CHECK_EQ(t, "(");             t=nextToken(in);
-  CHECK_EQ(t, ")");             t=nextToken(in);
-  CHECK_EQ(t, "'");             t=nextToken(in);
-  CHECK_EQ(t, "a");            t=nextToken(in);
+  readAll(in);
+  checkTraceContents("tokenize", "(\n)\n'\na\n\n");   // extra newline just an artifact
 }
 
 void test_tokenize_ends_comment_at_newline() {
   stringstream in("#abc def ghi\nabc");
-  Token t = nextToken(in);
-  CHECK_EQ(t, "abc");           t=nextToken(in);
+  readAll(in);
+  checkTraceContents("tokenize", "abc\n");
 }
 
 void test_tokenize_suppresses_comments() {
   stringstream in("abc\n#abc\ndef\nghi");
-  Token t = nextToken(in);
-  CHECK_EQ(t, "abc");           t=nextToken(in);
-  CHECK_EQ(t, "def");           t=nextToken(in);
-  CHECK_EQ(t, "ghi");           t=nextToken(in);
+  readAll(in);
+  checkTraceContents("tokenize", "abc\ndef\nghi\n");
 }
 
 void test_tokenize_suppresses_comments2() {
   stringstream in("a b\n  c\n#abc\ndef\n  ghi\n\njkl");
-  Token t = nextToken(in);
-  CHECK_EQ(t, "a");             t=nextToken(in);
-  CHECK_EQ(t, "b");             t=nextToken(in);
-  CHECK_EQ(t, "c");             t=nextToken(in);
-  CHECK_EQ(t, "def");           t=nextToken(in);
-  CHECK_EQ(t, "ghi");           t=nextToken(in);
-  CHECK_EQ(t, "jkl");           t=nextToken(in);
+  readAll(in);
+  checkTraceContents("tokenize", "a\nb\nc\ndef\nghi\njkl\n");
 }
 
 void test_tokenize_suppresses_trailing_whitespace() {
   stringstream in("a \nb\r\nc");
-  Token t = nextToken(in);
-  CHECK_EQ(t, "a");             t=nextToken(in);
-  CHECK_EQ(t, "b");             t=nextToken(in);
-  CHECK_EQ(t, "c");             t=nextToken(in);
+  readAll(in);
+  checkTraceContents("tokenize", "a\nb\nc\n");
 }
 
 void test_tokenize_suppresses_repeated_newline() {
   stringstream in("34\n\n\"abc \\\"quote def\"");
-  Token t = nextToken(in);
-  CHECK_EQ(t, "34");            t=nextToken(in);
-  CHECK_EQ(t, "\"abc \\\"quote def\"");
+  readAll(in);
+  checkTraceContents("tokenize", "34\n\"abc \\\"quote def\"\n");
 }
 
 void test_tokenize_suppresses_whitespace_lines() {
   stringstream in("abc def ghi\n\n    \n  def");
-  Token t = nextToken(in);
-  CHECK_EQ(t, "abc");           t=nextToken(in);
-  CHECK_EQ(t, "def");           t=nextToken(in);
-  CHECK_EQ(t, "ghi");           t=nextToken(in);
-  CHECK_EQ(t, "def");           t=nextToken(in);
+  readAll(in);
+  checkTraceContents("tokenize", "abc\ndef\nghi\ndef\n");
 }
 
 void test_tokenize_suppresses_whitespace_lines2() {
   stringstream in("  \nabc def ghi\n\n    \n  def");
-  Token t = nextToken(in);
-  CHECK_EQ(t, "abc");           t=nextToken(in);
-  CHECK_EQ(t, "def");           t=nextToken(in);
-  CHECK_EQ(t, "ghi");           t=nextToken(in);
-  CHECK_EQ(t, "def");           t=nextToken(in);
+  readAll(in);
+  checkTraceContents("tokenize", "abc\ndef\nghi\ndef\n");
 }
