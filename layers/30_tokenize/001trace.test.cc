aaec2c1505cbb12638ba7a64794da0017b8f88fc
Kartik K. Agaram <vc@akkartik.com>
2013-05-16 18:27:13 -0700

    2771

--- layers/30_tokenize/001trace.test.cc
+++ layers/30_tokenize/001trace.test.cc
@@ -1,9 +1,11 @@
 void test_trace_check_compares() {
+  checkTraceContents("test layer", "");
   trace("test layer") << "foo\n";
   checkTraceContents("test layer", "foo\n");
 }
 
 void test_trace_check_filters_layers() {
+  checkTraceContents("test layer", "");
   trace("test layer 1") << "foo\n";
   trace("test layer 2") << "bar\n";
   checkTraceContents("test layer 1", "foo\n");
