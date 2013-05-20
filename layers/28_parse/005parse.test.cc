26119b68b5756736de0676544525c68176be8546
Kartik K. Agaram <vc@akkartik.com>
2013-05-19 17:12:38 -0700

    2788 - done tracifying parse
    Another bit of ugliness is the trailing paren in nested calls.

--- layers/28_parse/005parse.test.cc
+++ layers/28_parse/005parse.test.cc
@@ -30,44 +30,15 @@ void test_parse_handles_forms() {
 
 void test_parse_handles_nested_forms() {
   stringstream in("(34 (2 3) \"a b c\")");
-  AstNode n = nextAstNode(in);
-  CHECK(!n.elems.empty());
-  list<AstNode>::iterator p = n.elems.begin();
-  CHECK_EQ(*p, Token("(")); ++p;
-  CHECK_EQ(*p, Token("34")); ++p;
-  CHECK(!p->elems.empty());
-    list<AstNode> ast2 = p->elems; ++p;
-    list<AstNode>::iterator q = ast2.begin();
-    CHECK_EQ(*q, Token("(")); ++q;
-    CHECK_EQ(*q, Token("2")); ++q;
-    CHECK_EQ(*q, Token("3")); ++q;
-    CHECK_EQ(*q, Token(")")); ++q;
-    CHECK(q == ast2.end());
-
-  CHECK_EQ(*p, Token("\"a b c\"")); ++p;
-  CHECK_EQ(*p, Token(")")); ++p;
-  CHECK(p == n.elems.end());
-  CHECK_EQ(nextAstNode(in), "");
+  readAll(in);
+  checkTraceContents2("parse", 1, "(34 (2 3) \"a b c\")\n");
+  checkTraceContents2("parse", 2, "34\n(2 3)\n\"a b c\"\n)\n");
+  checkTraceContents2("parse", 3, "2\n3\n)\n");
 }
 
 void test_parse_handles_nested_forms_with_comments() {
   stringstream in("(a b (c d #\n))");
-  AstNode n = nextAstNode(in);
-  CHECK(!n.elems.empty());
-  list<AstNode>::iterator p = n.elems.begin();
-  CHECK_EQ(*p, Token("(")); ++p;
-  CHECK_EQ(*p, Token("a")); ++p;
-  CHECK_EQ(*p, Token("b")); ++p;
-  CHECK(!p->elems.empty());
-    list<AstNode> ast2 = p->elems; ++p;
-    list<AstNode>::iterator q = ast2.begin();
-    CHECK_EQ(*q, Token("(")); ++q;
-    CHECK_EQ(*q, Token("c")); ++q;
-    CHECK_EQ(*q, Token("d")); ++q;
-    CHECK_EQ(*q, Token(")")); ++q;
-    CHECK(q == ast2.end());
-
-  CHECK_EQ(*p, Token(")")); ++p;
-  CHECK(p == n.elems.end());
-  CHECK_EQ(nextAstNode(in), "");
+  readAll(in);
+  checkTraceContents2("parse", 1, "(a b (c d))\n");
+  checkTraceContents2("parse", 2, "a\nb\n(c d)\n)\n");
 }
