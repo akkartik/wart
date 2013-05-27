87c3ddcd6cd8d0a90fa7b6673e9bd3fd9ab3a4b6
Kartik K. Agaram <vc@akkartik.com>
2013-05-27 13:47:24 -0700

    2821 - fixed; string(...size) not string(...end)
    But there's still a bug here: really long strings may drop characters if
    NOT_FOUND-begin < size.
    
    Is it a problem that this is hard to test? Or can I just blame
    strings as poorly designed for testability?

--- layers/20_no_comment_tokens/001trace.cc
+++ layers/20_no_comment_tokens/001trace.cc
@@ -153,7 +153,8 @@ vector<string> split(string s, char delim) {
   string::size_type begin=0, end=s.find(delim);
   while (true) {
     trace("string split") << begin << '-' << end << '\n';
-    result.push_back(string(s, begin, end));
+    trace("string split: inserting") << string(s, begin, end-begin) << '\n';
+    result.push_back(string(s, begin, end-begin));
     if (end == NOT_FOUND) break;
     begin = end+1;
     end = s.find(delim, begin);
