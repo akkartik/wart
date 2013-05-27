3ebaa63e2ce5bb00a8befd48d7a6ef782231471c
Kartik K. Agaram <vc@akkartik.com>
2013-05-27 11:31:56 -0700

    2811

--- layers/20_no_comment_tokens/021eval.test.cc
+++ layers/20_no_comment_tokens/021eval.test.cc
@@ -50,133 +50,128 @@ void test_evalBindAll_handles_destructured_params() {
 
 
 void test_nil_evals_to_itself() {
-  Cell* expr = read("()");
-  Cell* result = eval(expr);
-  CHECK_EQ(result, nil);
+  stringstream in("()");
+  run(in);
   checkTraceContents("eval", "nil\n=> nil\n");
 }
 
 void test_num_evals_to_itself() {
-  Cell* expr = read("34");
-  Cell* result = eval(expr);
-  CHECK_EQ(result, expr);
+  stringstream in("34");
+  run(in);
   checkTraceContents("eval", "34\nliteral: 34\n");
 }
 
 void test_colonsym_evals_to_itself() {
-  Cell* expr = read(":abc");
-  Cell* result = eval(expr);
-  CHECK_EQ(result, expr);
+  stringstream in(":abc");
+  run(in);
   checkTraceContents("eval", ":abc\nkeyword sym: :abc\n");
 }
 
 void test_colon_evals() {
-  Cell* expr = read(":");
+  stringstream in(":");
   newBinding(":", newNum(34));
-  Cell* result = eval(expr);
-  CHECK_EQ(result, newNum(34));
+  run(in);
   checkTraceContents("eval", ":\nsym: 34\n");
 }
 
 void test_string_evals_to_itself() {
-  Cell* expr = read("\"ac bd\"");
-  Cell* result = eval(expr);
-  CHECK_EQ(result, expr);
+  stringstream in("\"ac bd\"");
+  run(in);
   checkTraceContents("eval", "\"ac bd\"\nliteral: \"ac bd\"\n");
 }
 
 void test_sym_evals_to_value() {
   newBinding("a", newNum(34));
-  Cell* expr = read("a");
-  Cell* result = eval(expr);
-  CHECK_EQ(result, newNum(34));
+  stringstream in("a");
+  run(in);
   checkTraceContents("eval", "a\nsym: 34\n");
 }
 
 void test_sym_evals_to_itself() {
   newBinding("a", newSym("a"));
-  Cell* expr = read("a");
-  Cell* result = eval(expr);
-  CHECK_EQ(result, expr);
+  stringstream in("a");
+  run(in);
   checkTraceContents("eval", "a\nsym: a\n");
 }
 
 void test_eval_handles_quoted_atoms() {
   stringstream in("'a '34");
-  while (!eof(in))
-    eval(read(in));
+  run(in);
   checkTraceContents("eval", "'a\nquote: a\n'34\nquote: 34\n");
 }
 
 void test_eval_handles_quoted_lists() {
-  Cell* expr = read("'(a b)");
-  eval(expr);
+  stringstream in("'(a b)");
+  run(in);
   checkTraceContents("eval", "'(a b)\nquote: (a b)\n");
 }
 
 void test_eval_handles_rest_params() {
-  Cell* call = read("((fn (a b ... c) c) 1 2 3 4 5)");
-  eval(call);
+  stringstream in("((fn (a b ... c) c) 1 2 3 4 5)");
+  run(in);
   checkTraceContents2("eval", 1, "((fn (a b ... c) c) 1 2 3 4 5)\n=> (3 4 5)\n");
 }
 
 void test_eval_handles_fn_calls() {
-  Cell* call = read("((fn () 34))");
-  eval(call);
+  stringstream in("((fn () 34))");
+  run(in);
   checkTraceContents2("eval", 1, "((fn nil 34))\n=> 34\n");
 }
 
 void test_eval_expands_syms_in_fn_bodies() {
-  Cell* call = read("((fn () a))");
+  stringstream in("((fn () a))");
   newBinding("a", newNum(34));
-  eval(call);
+  run(in);
   checkTraceContents2("eval", 1, "((fn nil a))\n=> 34\n");
 }
 
 void test_eval_handles_assigned_fn_calls() {
-  Cell* fn = read("(fn () 34)");
-  Cell* f = eval(fn);
-  newBinding("f", f);
-  delete global_trace_stream, global_trace_stream = new TraceStream;
-    Cell* call = read("(f)");
-    eval(call);
-    checkTraceContents2("eval", 1, "(f)\n=> 34\n");
+  {
+    stringstream in("(<- f (fn () 34))");
+    run(in);
+    delete global_trace_stream, global_trace_stream = new TraceStream;
+  }
+  stringstream in("(f)");
+  run(in);
+  checkTraceContents2("eval", 1, "(f)\n=> 34\n");
 }
 
 void test_eval_handles_multiple_args() {
-  Cell* fn = read("(fn (a b) b)");
-  Cell* f = eval(fn);
-  newBinding("f", f);
-  delete global_trace_stream, global_trace_stream = new TraceStream;
-  Cell* call = read("(f 1 2)");
-  eval(call);
+  {
+    stringstream in("(<- f (fn (a b) b))");
+    run(in);
+    delete global_trace_stream, global_trace_stream = new TraceStream;
+  }
+  stringstream in("(f 1 2)");
+  run(in);
   checkTraceContents2("eval", 1, "(f 1 2)\n=> 2\n");
 }
 
 void test_eval_handles_multiple_body_exprs() {
-  Cell* fn = read("(fn () 1 2)");
-  Cell* f = eval(fn);
-  newBinding("f", f);
-  delete global_trace_stream, global_trace_stream = new TraceStream;
-  Cell* call = read("(f)");
-  eval(call);
+  {
+    stringstream in("(<- f (fn () 1 2))");
+    run(in);
+    delete global_trace_stream, global_trace_stream = new TraceStream;
+  }
+  stringstream in("(f)");
+  run(in);
   checkTraceContents2("eval", 1, "(f)\n=> 2\n");
 }
 
 void test_eval_handles_vararg_param() {
-  Cell* call = read("((fn args args) 1)");
-  eval(call);
+  stringstream in("((fn args args) 1)");
+  run(in);
   checkTraceContents2("eval", 1, "((fn args args) 1)\n=> (1)\n");
 }
 
 void test_eval_evals_args() {
-  Cell* call = read("((fn (f) (f)) (fn () 34))");
-  eval(call);
+  stringstream in("((fn (f) (f)) (fn () 34))");
+  run(in);
   checkTraceContents2("eval", 1, "((fn (f) (f)) (fn nil 34))\n=> 34\n");
 }
 
 void test_eval_handles_destructured_params() {
-  Cell* call = read("((fn ((a b)) b) '(1 2))");
-  eval(call);
+  stringstream in("((fn ((a b)) b) '(1 2))");
+  run(in);
   checkTraceContents2("eval", 1, "((fn ((a b)) b) '(1 2))\n=> 2\n");
 }
