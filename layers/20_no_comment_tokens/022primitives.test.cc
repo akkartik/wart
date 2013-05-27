705b9b6dd5e5e814190919d2e4c465764b02ee4e
Kartik K. Agaram <vc@akkartik.com>
2013-05-27 16:19:43 -0700

    2840

--- layers/20_no_comment_tokens/022primitives.test.cc
+++ layers/20_no_comment_tokens/022primitives.test.cc
@@ -1,6 +1,5 @@
 void test_fn_works() {
-  Cell* fn = read("(fn(x) x)");
-  Cell* result = eval(fn);
+  Cell* result = run("(fn(x) x)");
   // {sig: (x), body: (x)}
   CHECK_EQ(car(get(result, sym_sig)), newSym("x"));
   CHECK_EQ(cdr(get(result, sym_sig)), nil);
@@ -11,90 +10,77 @@ void test_fn_works() {
 
 
 void test_if_sees_args_in_then_and_else() {
-  Cell* fn = read("(fn(x) (if 34 x))");
-  Cell* f = eval(fn);
-  newBinding("f", f);
-  Cell* call = read("(f 35)");
-  Cell* result = eval(call);
-  CHECK_EQ(result, newNum(35));
+  run("(<- f (fn(x) (if 34 x)))");
+  CLEAR_TRACE;
+  run("(f 35)");
+  CHECK_TRACE_CONTENTS("eval", "=> 35\n");
 }
 
 void test_not_works() {
-  Cell* call = read("(not 35)");
-  Cell* result = eval(call);
-  CHECK_EQ(result, nil);
+  run("(not 35)");
+  CHECK_TRACE_CONTENTS("eval", "=> compiled fn: nil\n");
 }
 
 void test_not_works2() {
-  Cell* call = read("(not nil)");
-  Cell* result = eval(call);
-  CHECK_EQ(result, newNum(1));
+  run("(not nil)");
+  CHECK_TRACE_CONTENTS("eval", "=> compiled fn: 1\n");
 }
 
 void test_cons_works() {
-  Cell* call = read("(cons 1 2)");
-  Cell* result = eval(call);
-  // (1 ... 2)
-  CHECK_EQ(car(result), newNum(1));
-  CHECK_EQ(cdr(result), newNum(2));
+  run("(cons 1 2)");
+  CHECK_TRACE_CONTENTS("eval", "=> compiled fn: (1 ... 2)\n");
 }
 
 void test_assign_to_non_sym_warns() {
-  Cell* expr = read("(<- 3 nil)");
-  eval(expr);
+  run("(<- 3 nil)");
   CHECK_EQ(raiseCount, 1);   raiseCount=0;
 }
 
 void test_assign_lexical_var() {
-  Cell* fn = read("((fn(x) (<- x 34) x))");
-  Cell* call = eval(fn);
-  CHECK_EQ(call, newNum(34));
+  run("((fn (x) (<- x 34) x))");
+  CHECK_TRACE_CONTENTS("eval", "=> 34\n");
 }
 
 void test_assign_overrides_dynamic_vars() {
-  Cell* init1 = read("(<- x 3)");
-  eval(init1);
-  Cell* init2 = read("(<- x 5)");
-  eval(init2);
-  CHECK_EQ(lookup("x"), newNum(5));
+  run("(<- x 3)");
+  run("(<- x 5)");
+  CLEAR_TRACE;
+  run("x");
+  CHECK_TRACE_CONTENTS("eval", "=> sym: 5\n");
 }
 
 void test_assign_overrides_within_lexical_scope() {
-  Cell* init1 = read("(<- x 3)");
-  eval(init1);
-  Cell* init2 = read("((fn() (<- x 5)))");
-  eval(init2);
-  CHECK_EQ(lookup("x"), newNum(5));
+  run("(<- x 3)");
+  run("((fn () (<- x 5)))");
+  CLEAR_TRACE;
+  run("x");
+  CHECK_TRACE_CONTENTS("eval", "=> sym: 5\n");
 }
 
 void test_assign_never_overrides_lexical_vars_in_caller_scope() {
-  Cell* fn = read("((fn(x) (<- y x)) 34)");
-  eval(fn);
-  CHECK_EQ(lookup("y"), newNum(34));
+  run("((fn (x) (<- y x)) 34)");
+  CLEAR_TRACE;
+  run("y");
+  CHECK_TRACE_CONTENTS("eval", "=> sym: 34\n");
 }
 
 void test_assign_overrides_lexical_var() {
-  Cell* fn = read("((fn(x) (<- x 35) (<- x 36) x) 34)");
-  Cell* call = eval(fn);
-  CHECK_EQ(call, newNum(36));
+  run("((fn (x) (<- x 35) (<- x 36) x) 34)");
+  CHECK_TRACE_CONTENTS("eval", "=> 36\n");
 }
 
 void test_equal_handles_nil() {
-  Cell* call = read("(= nil nil)");
-  Cell* result = eval(call);
-  CHECK(result);
-  CHECK(result != nil);
+  run("(= nil nil)");
+  CHECK_TRACE_CONTENTS("eval", "=> compiled fn: 1\n");
 }
 
 void test_equal_handles_floats() {
-  Cell* call = read("(= (/ 3.0 2) 1.5)");
-  Cell* result = eval(call);
-  CHECK(result);
-  CHECK(result != nil);
+  run("(= (/ 3.0 2) 1.5)");
+  CHECK_TRACE_CONTENTS("eval", "=> compiled fn: 1.5\n");
 }
 
 void test_equal_handles_float_vs_nil() {
-  Cell* call = read("(= nil 1.5)");
-  eval(call);
+  run("(= nil 1.5)");
   CHECK_EQ(raiseCount, 0);
+  CHECK_TRACE_CONTENTS("eval", "=> compiled fn: nil\n");
 }
