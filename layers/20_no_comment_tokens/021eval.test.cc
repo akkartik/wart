7b5cbd93b7a0adde4f894d88a7b757eb828cadbd
Kartik K. Agaram <vc@akkartik.com>
2013-05-27 16:25:39 -0700

    2841 - no need to check inputs in trace
    Just make sure we specify the top-level eval, so we don't
    pass on intermediate nils, etc.

--- layers/20_no_comment_tokens/021eval.test.cc
+++ layers/20_no_comment_tokens/021eval.test.cc
@@ -41,40 +41,40 @@ void test_evalBindAll_handles_destructured_params() {
 
 void test_nil_evals_to_itself() {
   run("()");
-  CHECK_TRACE_CONTENTS("eval", "nil\n=> nil\n");
+  CHECK_TRACE_CONTENTS("eval", 1, "=> nil\n");
 }
 
 void test_num_evals_to_itself() {
   run("34");
-  CHECK_TRACE_CONTENTS("eval", "34\n=> literal: 34\n");
+  CHECK_TRACE_CONTENTS("eval", 1, "=> literal: 34\n");
 }
 
 void test_colonsym_evals_to_itself() {
   run(":abc");
-  CHECK_TRACE_CONTENTS("eval", ":abc\n=> keyword sym: :abc\n");
+  CHECK_TRACE_CONTENTS("eval", 1, "=> keyword sym: :abc\n");
 }
 
 void test_colon_evals() {
   newBinding(":", newNum(34));
   run(":");
-  CHECK_TRACE_CONTENTS("eval", ":\n=> sym: 34\n");
+  CHECK_TRACE_CONTENTS("eval", 1, "=> sym: 34\n");
 }
 
 void test_string_evals_to_itself() {
   run("\"ac bd\"");
-  CHECK_TRACE_CONTENTS("eval", "\"ac bd\"\n=> literal: \"ac bd\"\n");
+  CHECK_TRACE_CONTENTS("eval", 1, "=> literal: \"ac bd\"\n");
 }
 
 void test_sym_evals_to_value() {
   newBinding("a", newNum(34));
   run("a");
-  CHECK_TRACE_CONTENTS("eval", "a\n=> sym: 34\n");
+  CHECK_TRACE_CONTENTS("eval", 1, "=> sym: 34\n");
 }
 
 void test_sym_evals_to_itself() {
   newBinding("a", newSym("a"));
   run("a");
-  CHECK_TRACE_CONTENTS("eval", "a\n=> sym: a\n");
+  CHECK_TRACE_CONTENTS("eval", 1, "=> sym: a\n");
 }
 
 void test_eval_handles_quoted_atoms() {
@@ -84,57 +84,57 @@ void test_eval_handles_quoted_atoms() {
 
 void test_eval_handles_quoted_lists() {
   run("'(a b)");
-  CHECK_TRACE_CONTENTS("eval", "'(a b)\n=> quote: (a b)\n");
+  CHECK_TRACE_CONTENTS("eval", 1, "=> quote: (a b)\n");
 }
 
 void test_eval_handles_rest_params() {
   run("((fn (a b ... c) c) 1 2 3 4 5)");
-  CHECK_TRACE_CONTENTS("eval", 1, "((fn (a b ... c) c) 1 2 3 4 5)\n=> (3 4 5)\n");
+  CHECK_TRACE_CONTENTS("eval", 1, "=> (3 4 5)\n");
 }
 
 void test_eval_handles_fn_calls() {
   run("((fn () 34))");
-  CHECK_TRACE_CONTENTS("eval", 1, "((fn nil 34))\n=> 34\n");
+  CHECK_TRACE_CONTENTS("eval", 1, "=> 34\n");
 }
 
 void test_eval_expands_syms_in_fn_bodies() {
   newBinding("a", newNum(34));
   run("((fn () a))");
-  CHECK_TRACE_CONTENTS("eval", 1, "((fn nil a))\n=> 34\n");
+  CHECK_TRACE_CONTENTS("eval", 1, "=> 34\n");
 }
 
 void test_eval_handles_assigned_fn_calls() {
   run("(<- f (fn () 34))");
   CLEAR_TRACE;
   run("(f)");
-  CHECK_TRACE_CONTENTS("eval", 1, "(f)\n=> 34\n");
+  CHECK_TRACE_CONTENTS("eval", 1, "=> 34\n");
 }
 
 void test_eval_handles_multiple_args() {
   run("(<- f (fn (a b) b))");
   CLEAR_TRACE;
   run("(f 1 2)");
-  CHECK_TRACE_CONTENTS("eval", 1, "(f 1 2)\n=> 2\n");
+  CHECK_TRACE_CONTENTS("eval", 1, "=> 2\n");
 }
 
 void test_eval_handles_multiple_body_exprs() {
   run("(<- f (fn () 1 2))");
   CLEAR_TRACE;
   run("(f)");
-  CHECK_TRACE_CONTENTS("eval", 1, "(f)\n=> 2\n");
+  CHECK_TRACE_CONTENTS("eval", 1, "=> 2\n");
 }
 
 void test_eval_handles_vararg_param() {
   run("((fn args args) 1)");
-  CHECK_TRACE_CONTENTS("eval", 1, "((fn args args) 1)\n=> (1)\n");
+  CHECK_TRACE_CONTENTS("eval", 1, "=> (1)\n");
 }
 
 void test_eval_evals_args() {
   run("((fn (f) (f)) (fn () 34))");
-  CHECK_TRACE_CONTENTS("eval", 1, "((fn (f) (f)) (fn nil 34))\n=> 34\n");
+  CHECK_TRACE_CONTENTS("eval", 1, "=> 34\n");
 }
 
 void test_eval_handles_destructured_params() {
   run("((fn ((a b)) b) '(1 2))");
-  CHECK_TRACE_CONTENTS("eval", 1, "((fn ((a b)) b) '(1 2))\n=> 2\n");
+  CHECK_TRACE_CONTENTS("eval", 1, "=> 2\n");
 }
