7ba5c17d20d59512c36fabf4ee3268c1caeede95
Kartik K. Agaram <vc@akkartik.com>
2013-05-28 18:14:56 -0700

    2851
    Bugfix: can't check absence of multiple lines in trace.

--- layers/20_no_comment_tokens/022primitives.test.cc
+++ layers/20_no_comment_tokens/022primitives.test.cc
@@ -71,12 +71,12 @@ void test_assign_overrides_lexical_var() {
 
 void test_equal_handles_nil() {
   run("(= nil nil)");
-  CHECK_TRACE_DOESNT_CONTAIN("eval", 1, "compiled fn\n=> nil\n");
+  CHECK_TRACE_DOESNT_CONTAIN("eval", 1, "=> nil\n");
 }
 
 void test_equal_handles_floats() {
   run("(= (/ 3.0 2) 1.5)");
-  CHECK_TRACE_DOESNT_CONTAIN("eval", 1, "compiled fn\n=> nil\n");
+  CHECK_TRACE_DOESNT_CONTAIN("eval", 1, "=> nil\n");
 }
 
 void test_equal_handles_float_vs_nil() {
