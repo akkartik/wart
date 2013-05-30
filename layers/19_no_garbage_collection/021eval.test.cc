3e521b4f6fb1a5d640c423ccd9152b215b85d203
Kartik K. Agaram <vc@akkartik.com>
2013-05-30 11:35:21 -0700

    2879

--- layers/19_no_garbage_collection/021eval.test.cc
+++ layers/19_no_garbage_collection/021eval.test.cc
@@ -1,44 +1,3 @@
-void test_eval_bind_all_handles_unquoted_param() {
-  new_binding("a", new_num(3));
-  CLEAR_TRACE;
-  run("((fn (x)) a)");
-  CHECK_TRACE_CONTENTS("bind", "x: 3\n");
-}
-
-void test_eval_bind_all_binds_missing_params() {
-  new_binding("a", new_num(3));
-  CLEAR_TRACE;
-  run("((fn (x y)) a)");
-  CHECK_TRACE_CONTENTS("bind", "x: 3\ny: nil\n");
-}
-
-void test_eval_bind_all_handles_varargs_param() {
-  new_binding("a", new_num(3));
-  new_binding("b", new_num(4));
-  CLEAR_TRACE;
-  run("((fn x) a b)");
-  CHECK_TRACE_CONTENTS("eval", "a\nsym\n=> 3\nb\nsym\n=> 4\n");
-  CHECK_TRACE_CONTENTS("bind", "x: (3 4)\n");
-}
-
-void test_eval_bind_all_handles_rest_param() {
-  new_binding("a", new_num(3));
-  new_binding("b", new_num(4));
-  CLEAR_TRACE;
-  run("((fn (x ... y)) a b)");
-  CHECK_TRACE_CONTENTS("bind", "x: 3\ny: (4)\n");
-}
-
-void test_eval_bind_all_handles_destructured_params() {
-  new_binding("x", new_num(3));
-  new_binding("y", new_num(4));
-  CLEAR_TRACE;
-  run("((fn ((a b))) (cons x (cons y)))");
-  CHECK_TRACE_CONTENTS("bind", "a: 3\nb: 4\n");
-}
-
-
-
 void test_nil_evals_to_itself() {
   run("()");
   CHECK_TRACE_TOP("eval", "nil branch\n=> nil");
@@ -81,10 +40,7 @@ void test_eval_handles_quoted_lists() {
   CHECK_TRACE_TOP("eval", "quote\n=> (a b)\n");
 }
 
-void test_eval_handles_rest_params() {
-  run("((fn (a b ... c) c) 1 2 3 4 5)");
-  CHECK_TRACE_TOP("eval", "=> (3 4 5)\n");
-}
+
 
 void test_eval_handles_fn_calls() {
   run("((fn () 34))");
@@ -97,25 +53,33 @@ void test_eval_expands_syms_in_fn_bodies() {
   CHECK_TRACE_TOP("eval", "=> 34\n");
 }
 
-void test_eval_handles_assigned_fn_calls() {
-  run("(<- f (fn () 34))");
+void test_eval_handles_multiple_body_exprs() {
+  run("((fn () 1 2))");
+  CHECK_TRACE_TOP("eval", "=> 2\n");
+}
+
+void test_eval_evals_arg() {
+  new_binding("a", new_num(3));
   CLEAR_TRACE;
-  run("(f)");
+  run("((fn (x)) a)");
+  CHECK_TRACE_CONTENTS("bind", "x: 3\n");
+}
+
+void test_eval_evals_arg2() {
+  run("((fn (f) (f)) (fn () 34))");
   CHECK_TRACE_TOP("eval", "=> 34\n");
 }
 
 void test_eval_handles_multiple_args() {
-  run("(<- f (fn (a b) b))");
-  CLEAR_TRACE;
-  run("(f 1 2)");
+  run("((fn (a b) b) 1 2)");
   CHECK_TRACE_TOP("eval", "=> 2\n");
 }
 
-void test_eval_handles_multiple_body_exprs() {
-  run("(<- f (fn () 1 2))");
+void test_eval_binds_missing_params() {
+  new_binding("a", new_num(3));
   CLEAR_TRACE;
-  run("(f)");
-  CHECK_TRACE_TOP("eval", "=> 2\n");
+  run("((fn (x y)) a)");
+  CHECK_TRACE_CONTENTS("bind", "x: 3\ny: nil\n");
 }
 
 void test_eval_handles_vararg_param() {
@@ -123,12 +87,46 @@ void test_eval_handles_vararg_param() {
   CHECK_TRACE_TOP("eval", "=> (1)\n");
 }
 
-void test_eval_evals_args() {
-  run("((fn (f) (f)) (fn () 34))");
-  CHECK_TRACE_TOP("eval", "=> 34\n");
+void test_eval_evals_vararg_args() {
+  new_binding("a", new_num(3));
+  new_binding("b", new_num(4));
+  CLEAR_TRACE;
+  run("((fn x) a b)");
+  CHECK_TRACE_CONTENTS("eval", "a\nsym\n=> 3\nb\nsym\n=> 4\n");
+  CHECK_TRACE_CONTENTS("bind", "x: (3 4)\n");
+}
+
+void test_eval_handles_rest_params() {
+  run("((fn (a b ... c) c) 1 2 3 4 5)");
+  CHECK_TRACE_TOP("eval", "=> (3 4 5)\n");
+}
+
+void test_eval_evals_rest_args() {
+  new_binding("a", new_num(3));
+  new_binding("b", new_num(4));
+  CLEAR_TRACE;
+  run("((fn (x ... y)) a b)");
+  CHECK_TRACE_CONTENTS("bind", "x: 3\ny: (4)\n");
 }
 
 void test_eval_handles_destructured_params() {
   run("((fn ((a b)) b) '(1 2))");
-  CHECK_TRACE_TOP("eval", "=> 2\n");
+  CHECK_TRACE_CONTENTS("bind", "a: 1\nb: 2\n");
+}
+
+void test_eval_evals_destructured_args() {
+  new_binding("x", new_num(3));
+  new_binding("y", new_num(4));
+  CLEAR_TRACE;
+  run("((fn ((a b))) (cons x (cons y)))");
+  CHECK_TRACE_CONTENTS("bind", "a: 3\nb: 4\n");
+}
+
+
+
+void test_eval_handles_assigned_fn_calls() {
+  run("(<- f (fn () 34))");
+  CLEAR_TRACE;
+  run("(f)");
+  CHECK_TRACE_TOP("eval", "=> 34\n");
 }
