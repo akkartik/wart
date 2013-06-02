2c7e8bd26fa9102af74d68755da163e22072baea
Kartik K. Agaram <vc@akkartik.com>
2013-06-02 11:46:24 -0700

    2932 - tracify tests for list/string primitives

--- layers/04_no_param_aliases/024string.test.cc
+++ layers/04_no_param_aliases/024string.test.cc
@@ -1,25 +1,14 @@
 void test_string_range() {
-  cell* expr = read("(string_range \"abc\" 0 2)");
-  cell* result = eval(expr);
-  CHECK_EQ(to_string(result), "ab");
-  rmref(result);
-  rmref(expr);
+  run("(string_range \"abc\" 0 2)");
+  CHECK_TRACE_TOP("eval", "=> \"ab\"");
 }
 
 void test_string_compare() {
-  cell* expr = read("(string_lesser \"abc\" \"abd\")");
-  cell* result = eval(expr);
-  CHECK_EQ(result, new_num(1))
-  rmref(result);
-  rmref(expr);
+  run("(string_lesser \"abc\" \"abd\")");
+  CHECK_TRACE_TOP("eval", "=> 1");
 }
 
 void test_string_split() {
-  cell* expr = read("(split \"abc abd\")");
-  cell* result = eval(expr);
-  CHECK_EQ(to_string(car(result)), "abc");
-  CHECK_EQ(to_string(car(cdr(result))), "abd");
-  CHECK_EQ(cdr(cdr(result)), nil);
-  rmref(result);
-  rmref(expr);
+  run("(split \"abc abd\")");
+  CHECK_TRACE_TOP("eval", "=> (\"abc\" \"abd\")");
 }
