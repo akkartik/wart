94a7cb735cccd72ddf3fb31d88b8a2236d87e714
Kartik K. Agaram <vc@akkartik.com>
2013-12-20 17:25:20 -0800

    3323 - dump an entire hierarchy of trace layers

--- 001trace.cc
+++ 001trace.cc
@@ -21,9 +21,10 @@ struct trace_stream {
   void newline() {
     if (!curr_stream) return;
     past_lines.push_back(pair<string, pair<int, string> >(curr_layer, pair<int, string>(frame[curr_layer], curr_stream->str())));
-    if (curr_layer == dump_layer || curr_layer == "dump" ||
-        (!Hide_warnings && curr_layer == "warn"))
-      cerr << frame[curr_layer] << ": " << with_newline(curr_stream->str());
+    if ((!dump_layer.empty() && prefix_match(dump_layer, curr_layer))
+        || curr_layer == "dump"
+        || (!Hide_warnings && curr_layer == "warn"))
+      cerr << curr_layer << "/" << frame[curr_layer] << ": " << with_newline(curr_stream->str());
     delete curr_stream;
     curr_stream = NULL;
   }
