cb87a3b0da0993619d14571c6879c9f94747615d
Kartik K. Agaram <vc@akkartik.com>
2014-03-04 18:18:51 -0800

    3403

--- 002main.cc
+++ 002main.cc
@@ -1,20 +1,5 @@
 //// run unit tests or interactive interpreter
 
-// The design of lisp seems mindful of the following:
-//  keep programs as short as possible
-//  as programs grow, allow them to be decomposed into functions
-//  functions are defined with a recipe for work, and invoked to perform that work
-//  functions are parameterized with vars (parameters) and invoked with data (arguments)
-//  new functions can be defined at any time
-//  functions can invoke other functions before they've been defined
-//  functions are data; they can be passed to other functions
-//  when an invocation contains multiple functions, make the one being invoked easy to identify
-//    almost always the first symbol in the invocation
-//  there have to be *some* bedrock primitives, but any primitive can be user-defined in principle
-//
-// These weren't the reasons lisp was created; they're the reasons I attribute
-// to its power.
-
 bool Interactive = false;
 bool Warn_on_unknown_var = true;
 
